b56d8d4504b4526d4d9e41ed80d3613479c94502
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-10 21:54:47 +0200

    Create default.php

---
+++ default.php
@@ -0,0 +1,15 @@
+<!DOCTOR HTML PUBLIC "-//w3c//System HTML 4.01 Transitional//End" "http://www.w3c.org/Tr/HTML/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title> МОБИЛЬНАЯ ПАУТИНКА </title></head>
+<body><table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr><td valign="top" width="40%"><p><b>pautinka.mobi</b></p>
+<a href="/default-old.php"><img style="border: 1px solid black" width=23 height=17 src= "z5RUS.png"/> <a href="/default-2.php"><img style="border: 1px solid black" width=23 height=17 src="z6.gif"> <a href="/default-3.php"><img width=23 height=19 src= "z1.png"/></a><br/>&nbsp;</td><td width="60%"><img width=100% src= "pauk.png"/></td></tr>
+<tr><td colspan=2><i><span style="color: gray">Плети&nbsp;свою&nbsp;сеть</span></i><br/>&nbsp;</td></tr><br/>
+<tr><form name = "input" action ="form.php" method= "get"><td>
+E-mail:</td><td><input type = "text"/size=20></td></tr><tr><td>Password:</td><td><input type = "password"/size=20></td></tr><tr><td align=center colspan=2><br/><input type = "submit" value= "РЕГИСТРАЦИЯ / ВХОД"/><br/></td></form></tr>
+<tr><td colspan=2><br/>&nbsp;</td></tr>
+<tr><td colspan=2><span style="color:gray;"><hr/></span>
+&copy; 2014  m.pautinka.mobi</td></tr></table>
+</body>
+</html>
