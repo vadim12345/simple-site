ec6a63ca4abba00c6ad392f723d9f6f2dde72b12
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-10 22:01:33 +0200

    Create default1.php

---
+++ default1.php
@@ -0,0 +1,11 @@
+<!DOCTOR HTML PUBLIC "-//w3c//System HTML 4.01 Transitional//End" "http://www.w3c.org/Tr/HTML/loose.dtd">
+<html>
+<head>
+<meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ГЛАВНАЯ ПАУТИНКА </title></head>
+<body><table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr><td valign="top" width="40%"><b>pautinka.mobi<b></td></tr><tr><td><img width=100% src= "pauk1.jpg"/></td></tr><br/>
+<tr><td colspan=2><br/>&nbsp;</td></tr>
+<tr><td colspan=2><span style="color:gray;"><hr/></span>
+&copy; 2014  m.pautinka.mobi</td></tr></table>
+</body>
+</html>
