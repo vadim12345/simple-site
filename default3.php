680ceaae8c2bbc012de58641736db6eadd4751c0
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-11 16:55:54 +0200

    Create default3.php

---
+++ default3.php
@@ -0,0 +1,19 @@
+<!DOCTOR HTML PUBLIC "-//w3c//System HTML 4.01 Transitional//End" "http://www.w3c.org/Tr/HTML/loose.dtd">
+<html>
+<head>
+<meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ИНФОРМ ПАУТИНКА </title></head>
+<body><table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr><td valign="top" width="40%"><b>pautinka.mobi<b></td></tr><tr><td><img width=100% src= "pauk2.jpg"/></td></tr><br/><tr><td colspan=2>Как это работает?
+После регистрации Вы получите
+специальную реферальную ссылку.
+Разошлите её другим людям и как
+только они зарегистрируются на
+Hostinger.ru - Вам будут зачислены
+очки. Чем больше людей Вы
+пригласите, тем больше подарков
+получите!</td></tr>
+<tr><td colspan=2><br/>&nbsp;</td></tr>
+<tr><td colspan=2><span style="color:gray;"><hr/></span>
+&copy; 2014  m.pautinka.mobi</td></tr></table>
+</body>
+</html>
