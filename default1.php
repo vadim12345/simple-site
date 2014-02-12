bbacddfb9ac81b961c5dbee852cf15c8db349436
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-12 12:35:03 +0200

    Update default1.php

--- default1.php
+++ default1.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 <meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ГЛАВНАЯ ПАУТИНКА </title></head>
-<body style="padding: 0; margin:0;"><img width="100%" src="pauk1-1.jpg"/><br>
-<table width="100%" border="1" cellpadding="0" cellspacing="0">
+<body style="padding: 0; margin:0;">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
 <tr><td><b>pautinka.mobi<b>....</td></tr>
-<tr><td><img width="100%" src= "pauk1.jpg"/></td></tr>
+<tr><td><img width="100%" src= "pauk1-1.jpg"/></td></tr>
 <tr><td><br/>&nbsp;</td></tr>
 <tr><td><span style="color:gray;"><hr/></span>
 &copy; 2014  m.pautinka.mobi</td></tr></table>
