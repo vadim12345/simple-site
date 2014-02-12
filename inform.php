6d0472107d1101c1422c30dd09a8079df98bf41b
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-12 22:14:07 +0200

    Update inform.php

--- inform.php
+++ inform.php
@@ -7,21 +7,21 @@
 <td align="center" valign="middle"><a href="https://pautinka.mobi/main.php">rus</a> | <a href=
 "pautinka.mobi/default1.php">eng</a></td></tr>
 <tr><td colspan=2><img width="100%" src= "pauk2-1-2.jpg"/></td></tr>
-<tr><td colspan=2><br/>
-Как это работает?<br/>
+<tr><td colspan=2 align="center" valign="middle">
+<h3>Как это работает?</h3>
 После регистрации Вы получите
-специальную реферальную ссылку.<br/>
-<a href="pautinka.mobi">https://pautinka.mobi</a><br/>
+специальную реферальную ссылку.
+<a href="pautinka.mobi">https://pautinka.mobi</a>
 Разошлите её другим людям и как
 только они зарегистрируются на
-pautinka.mobi<br/> 
+pautinka.mobi
 Вам будут зачислен 1 ГОЛОС.
 Чем больше людей Вы пригласите, 
-тем больше ГОЛОСОВ получите!<br/>
-3 ГОЛОСА = 1 ПАУТИНКA<br/></td></tr>
+тем больше ГОЛОСОВ получите!
+3 ГОЛОСА = 1 ПАУТИНКA</td></tr>
 <tr><td colspan=2>&nbsp</td></tr>
 <tr><td colspan=2 align="center" valign="middle"><span stule="color:gray;"><hr/></span>
-&copy; 2014 <a href="https://pautinka.mobi/main.php">pautinka.mobi</a></td></tr><br/>
+&copy; 2014 <a href="https://pautinka.mobi/main.php">pautinka.mobi</a></td></tr>
 </table>
 </body>
 </html>
