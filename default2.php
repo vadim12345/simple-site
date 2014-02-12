47a3e0e4a401c36060acdce5c585fd0d5af5ba94
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-12 14:13:10 +0200

    Update default2.php

--- default2.php
+++ default2.php
@@ -2,18 +2,21 @@
 <html>
 <head>
 <meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ИНФОРМ ПАУТИНКА </title></head>
-<body><table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td valign="top" width="40%"><b>pautinka.mobi<b></td></tr><tr><td><img width=100% src= "pauk2.jpg"/></td></tr><br/><tr><td colspan=2>Как это работает?
+<body style = "padding:0; margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr><td><b>pautinka.mobi<b></td></tr>
+<tr><td><img width="100%" src= "pauk2-1.jpg"/></td></tr><br/>
+<tr><td>Как это работает?
 После регистрации Вы получите
 специальную реферальную ссылку.
 Разошлите её другим людям и как
 только они зарегистрируются на
-Hostinger.ru - Вам будут зачислены
-очки. Чем больше людей Вы
-пригласите  тем больше подарков
-получите!</td></tr>
-<tr><td colspan=2><br/>&nbsp;</td></tr>
-<tr><td colspan=2><span style="color:gray;"><hr/></span>
+pautinka.mobi - Вам будут зачислен
+1 ГОЛОС.
+Чем больше людей Вы пригласите, 
+тем больше ГОЛОСОВ получите!
+3 ГОЛОСА = 1 ПАУТИНКИ</td></tr>
+<tr><td><br/>&nbsp;</td></tr>
+<tr><td><span style="color:gray;"><hr/></span>
 &copy; 2014  m.pautinka.mobi</td></tr></table>
 </body>
 </html>
