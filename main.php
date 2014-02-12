094bc1da629620d0e9d6ab4ae313a9b46d56330c
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-12 22:00:46 +0200

    Update main.php

--- main.php
+++ main.php
@@ -9,7 +9,10 @@
 eng</a></td></tr>
 <tr><td colspan=2><img width="100%" src= "pauk1-1.jpg"/></td></tr>
 <tr><td colspan=2>&nbsp;</td></tr>
+<tr><td colspan=2 align="center" valign="middle"><p>Паутинка<p/> символизирует порядок и равновесие сил, 
+однако основное значение символа - связывать и удерживать.</td></tr>
+<tr><td colspan=2>&nbsp;</td></tr>
 <tr><td colspan=2 align="center" valign="middle"><span style="color:gray;"><hr/></span>&copy; 2014 <a href=
-"https://pautinka.mobi/inform.php">pautinka.mobi</a><br/></td></tr></table>
+"https://pautinka.mobi/inform.php">pautinka.mobi</a></td></tr></table>
 </body>
 </html>
