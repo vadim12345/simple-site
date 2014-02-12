fd9e120a67cc6149b67826da9d0e4af111b67916
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-12 21:07:46 +0200

    Update main.php

--- main.php
+++ main.php
@@ -4,12 +4,12 @@
 <meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ГЛАВНАЯ ПАУТИНКА </title></head>
 <body style="padding: 0; margin:0;">
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td align="center" valign="middle"><a href="https://pautinka.mobi"pautinka.mobi</a></td>
-<td align="center" valign="middle"><a href="htps://pautinka.mobi"rus</a> | <a href="https://pautinka.mobi/main.php"
+<tr><td align="center" valign="middle"><a href="https://pautinka.mobi">pautinka.mobi</a></td>
+<td align="center" valign="middle"><a href="htps://pautinka.mobi">rus</a> | <a href="https://pautinka.mobi/main.php">
 eng</a></td></tr>
 <tr><td colspan=2><img width="100%" src= "pauk1-1.jpg"/></td></tr>
 <tr><td colspan=2>&nbsp;</td></tr>
 <tr><td colspan=2 align="center" valign="middle"><span style="color:gray;"><hr/></span>&copy; 2014<a href=
-"https://pautinka.mobi/inform.php"pautinka.mobi</a><br/></td></tr></table>
+"https://pautinka.mobi/inform.php">pautinka.mobi</a><br/></td></tr></table>
 </body>
 </html>
