40928b1ef93701735c350a0e0b665cf9fd998c40
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-12 15:24:35 +0200

    Update default2.php

--- default2.php
+++ default2.php
@@ -3,9 +3,11 @@
 <head>
 <meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ИНФОРМ ПАУТИНКА </title></head>
 <body style = "padding:0; margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td><b>pautinka.mobi<b></td></tr>
-<tr><td><img width="100%" src= "pauk2-1-2.jpg"/></td></tr>
-<tr><td><br/>
+<tr><td align="center" valign="middle"><a href="pautinka.mobi/default1.php">pautinka.mobi</a><td/>
+<td align="center" valign="middle"><a href="pautinka.mobi/default1.php">rus</a> <a href="pautinka.mobi/default1.php">eng</a>
+</td></tr>
+<tr><td colspan=2><img width="100%" src= "pauk2-1-2.jpg"/></td></tr>
+<tr><td colspan=2><br/>
 Как это работает?<br/>
 После регистрации Вы получите
 специальную реферальную ссылку.
@@ -16,8 +18,8 @@ pautinka.mobi<br/>
 Чем больше людей Вы пригласите, 
 тем больше ГОЛОСОВ получите!<br/>
 3 ГОЛОСА = 1 ПАУТИНКA</td></tr>
-<tr><td><br/>&nbsp;</td></tr>
-<tr><td><span style="color:gray;"><hr/></span>
+<tr><td colspan=2><br/>&nbsp;</td></tr>
+<tr><td colspan=2><span style="color:gray;"><hr/></span>
 &copy; 2014  m.pautinka.mobi</td></tr></table>
 </body>
 </html>
