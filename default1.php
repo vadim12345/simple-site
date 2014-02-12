128b1c477b3202e3c5acbc968496eba7de4e8b1d
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-02-12 13:03:07 +0300

    Update default1.php

--- default1.php
+++ default1.php
@@ -3,9 +3,10 @@
 <head>
 <meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ГЛАВНАЯ ПАУТИНКА </title></head>
 <body><table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td valign="top" width="40%"><b>pautinka.mobi<b></td></tr><tr><td><img width=100% src= "pauk1.jpg"/></td></tr><br/>
-<tr><td colspan=2><br/>&nbsp;</td></tr>
-<tr><td colspan=2><span style="color:gray;"><hr/></span>
+<tr><td><b>pautinka.mobi<b></td></tr>
+<tr><td><img width=100% src= "pauk1.jpg"/></td></tr>
+<tr><td><br/>&nbsp;</td></tr>
+<tr><td><span style="color:gray;"><hr/></span>
 &copy; 2014  m.pautinka.mobi</td></tr></table>
 </body>
 </html>
