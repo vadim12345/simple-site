fd11092f5bc3693a66884a020dce0d341582f059
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-12 14:21:08 +0200

    Update default2.php

--- default2.php
+++ default2.php
@@ -4,8 +4,8 @@
 <meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ИНФОРМ ПАУТИНКА </title></head>
 <body style = "padding:0; margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
 <tr><td><b>pautinka.mobi<b></td></tr>
-<tr><td><img width="100%" src= "pauk2-1.jpg"/></td></tr><br/>
-<tr><td>Как это работает?
+<tr><td><img width="100%" src= "pauk2-1.jpg"/></td></tr>
+<tr><td><br/>Как это работает?
 После регистрации Вы получите
 специальную реферальную ссылку.
 Разошлите её другим людям и как
@@ -14,7 +14,7 @@ pautinka.mobi - Вам будут зачислен
 1 ГОЛОС.
 Чем больше людей Вы пригласите, 
 тем больше ГОЛОСОВ получите!
-3 ГОЛОСА = 1 ПАУТИНКИ</td></tr>
+3 ГОЛОСА = 1 ПАУТИНКA</td></tr>
 <tr><td><br/>&nbsp;</td></tr>
 <tr><td><span style="color:gray;"><hr/></span>
 &copy; 2014  m.pautinka.mobi</td></tr></table>
