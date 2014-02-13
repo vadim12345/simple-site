c5300a3e066926b3a1e34e61e300a6f3de731f9c
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-13 13:11:18 +0200

    Update default.php

--- default.php
+++ default.php
@@ -5,7 +5,7 @@
 <body style="padding:0;margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
 <tr><td align="center" valign="middle"><a href="pautinka.mobi">pautinka.mobi</a><br/>
 <a href="/pautinka.mobi/main.php">rus</a> | <a href="/pautinka.mobi/main.php">eng</a>><br/>
-&nbsp;</td><td align="center" valign="middle"><img src="pauk.png"/></td></tr>
+&nbsp;</td><td align="center" valign="middle" width="50%"><img src="pauk.png"/></td></tr>
 <tr><td colspan=2><i><span style="color: gray">Плети&nbsp;свою&nbsp;сеть</span></i><br/>&nbsp;</td></tr><br/>
 <tr><form name = "input" action ="form.php" method= "get"><td align="center" valign="middle">
 E-mail:</td><td><input type = "text"/size=20></td></tr><tr><td align="center" valign="middle">
