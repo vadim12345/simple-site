6c24f39bb46dd22d469d4faa5b5fe9116b5b1163
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-13 15:59:03 +0200

    Update default.php

--- default.php
+++ default.php
@@ -2,19 +2,27 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/><title> МОБИЛЬНАЯ ПАУТИНКА </title></head>
-<body style="padding:0;margin:0;"><table width="100%" border="1" cellpadding="0" cellspacing="0">
+
+<body style="padding:0;margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
 <tr><td align="center" valign="middle"><a href="/pautinka.mobi">pautinka.mobi</a></td><td align="center" valign="middle">
 <a href="/pautinka.mobi/main.php">rus</a> | <a href="/pautinka.mobi/main.php">eng</a></td></tr>
+
 <tr><td colspan="2" align="center" valign="middle"><img width="150" src="pauk.png"/></td></tr>
-<tr><td colspan="2" align="center" valign="middle"><i><span style="color: gray">Плети&nbsp;свою&nbsp;сеть</span></i><br/>
-</td></tr>
+
+<tr><td colspan="2" align="center" valign="middle"><i><span style="color: gray">Плети&nbsp;свою&nbsp;сеть</span></i>
+<br/></td></tr>
+
 <table width="100%" border="1" cellpadding="0" cellspacing="3"><tr><td colspan="2" aalign="center" valign="middle">
 <tr><form name = "input" action ="form.php" method= "get"><td align="right" valign="middle">E-mail:</td>
 <td><input type = "text"/size=16></td></tr><tr><td align="right" valign="middle">Password:</td>
 <td><input type = "password"/size=16></td></tr></table>
-<tr><td colspan="2"></td></tr>
-<tr><td colspan=2 align="center" valign="middle"><input type ="submit" value= "РЕГИСТРАЦИЯ / ВХОД"/><br/></td></form></tr>
+
 <tr><td colspan="2">&nbsp;</td></tr>
+
+<tr><td colspan="2" align="center" valign="middle"><input type ="submit" value= "РЕГИСТРАЦИЯ / ВХОД"/><br/></td></form></tr>
+
+<tr><td colspan="2">&nbsp;</td></tr>
+
 <tr><td colspan="2" align="center" valign="middle"><span style="color:gray;"><hr/></span>
 &copy; 2014 <a href="/pautinka.mobi">pautinka.mobi</a></td></tr></table>
 </body>
