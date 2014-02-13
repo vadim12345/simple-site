578ae610c51824c3c58c691b3d27112064bfca50
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-13 13:47:43 +0200

    Update default.php

--- default.php
+++ default.php
@@ -3,13 +3,13 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/><title> МОБИЛЬНАЯ ПАУТИНКА </title></head>
 <body style="padding:0;margin:0;"><table width="100%" border="1" cellpadding="0" cellspacing="0">
-<tr><td align="center" valign="middle"><a href="pautinka.mobi">pautinka.mobi</a><br/>
-<a href="/pautinka.mobi/main.php">rus</a> | <a href="/pautinka.mobi/main.php">eng</a>><br/>
-&nbsp;</td><td align="center" valign="middle" width="20%"><img src="pauk.png"/></td></tr>
+<tr><td align="center" valign="middle"><a href="pautinka.mobi">pautinka.mobi</a></td><td align="center" valign="middle">
+<a href="/pautinka.mobi/main.php">rus</a> | <a href="/pautinka.mobi/main.php">eng</a></td></tr>
+<tr><td colspan=2 align="center" valign="middle"><img src="pauk.png"/></td></tr>
 <tr><td colspan=2><i><span style="color: gray">Плети&nbsp;свою&nbsp;сеть</span></i><br/>&nbsp;</td></tr><br/>
-<tr><form name = "input" action ="form.php" method= "get"><td align="center" valign="middle">
-E-mail:</td><td><input type = "text"/size=20></td></tr><tr><td align="center" valign="middle">
-Password:</td><td><input type = "password"/size=20></td></tr><tr><td align="center" valign="middle" colspan=2><br/>
+<tr><form name = "input" action ="form.php" method= "get"><td align="right" valign="middle">E-mail:</td>
+<td><input type = "text"/size=20></td></tr><tr><td align="right" valign="middle">Password:</td>
+<td><input type = "password"/size=20></td></tr><tr><td colspan=2 align="center" valign="middle"><br/>
 <input type ="submit" value= "РЕГИСТРАЦИЯ / ВХОД"/><br/></td></form></tr>
 <tr><td colspan=2><br/>&nbsp;</td></tr>
 <tr><td colspan=2 align="center" valign="middle"><span style="color:gray;"><hr/></span>
