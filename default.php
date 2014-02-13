7fbd2207adcf8a9d646a70e947b3611b6ffdcbb0
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-13 16:28:14 +0200

    Update default.php

--- default.php
+++ default.php
@@ -11,15 +11,11 @@
 
 <tr><td colspan="2" align="center" valign="middle"><i><span style="color: gray">Плети&nbsp;свою&nbsp;сеть</span></i>
 <br/></td></tr>
-
-<table width="100%" border="1" cellpadding="0" cellspacing="3"><tr><td colspan="2" aalign="center" valign="middle">
-<tr><form name = "input" action ="form.php" method= "get"><td align="right" valign="middle">E-mail:</td>
+<tr><td colspan="2" align="center" valign="middle"><table width="100%" border="1" cellpadding="0" cellspacing="3">
+<form name = "input" action ="form.php" method= "get"></td><td align="right" valign="middle">E-mail:</td>
 <td><input type = "text"/size=16></td></tr><tr><td align="right" valign="middle">Password:</td>
-<td><input type = "password"/size=16></td></tr></table>
-
-<tr><td colspan="2">&nbsp;</td></tr>
-
-<tr><td colspan="2" align="center" valign="middle"><input type ="submit" value= "РЕГИСТРАЦИЯ / ВХОД"/><br/></td></form></tr>
+<td><input type = "password"/size=16></td></tr>
+<tr><td align="center" valign="middle"><input type ="submit" value= "РЕГИСТРАЦИЯ / ВХОД"/></td></form></tr></table>
 
 <tr><td colspan="2">&nbsp;</td></tr>
 
