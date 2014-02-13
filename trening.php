1b523777207d8de9f43623d89ed283018a2b4adf
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-13 17:24:33 +0200

    Create trening.php

---
+++ trening.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+</head>
+<body>
+<ul class= "menu" >
+<li><a href = "">Главная</a></li>
+<li><a href = "">О компании</a>
+<ul>
+<li><a href = "" >Производство</a></li>
+<li><a href = "" >Клиенты</a></li>
+<li><a href = "" >Для партнеров</a></li>
+<li><a href = "" >Для клиентов</a></li>
+<li><a href = "" >Реклама</a></li>
+</ul>
+</li>
+<li><a href = "">Услуги</a></li>
+<li><a href = "">Фотогалерея</a></li>
+<li><a href = "">Цены</a></li>
+<li><a href = "">Отзывы</a></li>
+<li><a href = "">Контакты</a></li>
+</ul>
+</body>
+</html>
