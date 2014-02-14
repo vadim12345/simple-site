d9b7609927748691f918cb279ddd4c7814538617
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-14 18:29:23 +0300

    Update main.php

--- main.php
+++ main.php
@@ -4,18 +4,18 @@
 <meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ГЛАВНАЯ ПАУТИНКА </title></head>
 <body style="padding: 0; margin:0;">
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td align="center" valign="middle"><a href="https://www.pautinka.mobi">pautinka.mobi</a></td>
-<td align="center" valign="middle"><a href="https://www.pautinka.mobi">rus</a> | <a href="https://www.pautinka.mobi/main.php">
+<tr><td align="center" valign="middle"><a href="http://www.pautinka.mobi">pautinka.mobi</a></td>
+<td align="center" valign="middle"><a href="http://www.pautinka.mobi">rus</a> | <a href="http://www.pautinka.mobi/main.php">
 eng</a></td></tr>
 <tr><td colspan=2><img width="100%" src= "pauk1-1.jpg"/></td></tr>
 
-<tr><td colspan=2 align="center" valig="midle"><a href="https://www.pautinka.mobi/">главная</a> | <a href=
-"https://www.pautinka.mobi/inform.php">информ</a> | <a href="https://www.pautinka.mobi/trening.php">торговля</a> | <a href=
-"https://www.pautinka.mobi">выход</a></td></tr>
+<tr><td colspan=2 align="center" valig="midle"><a href="http://www.pautinka.mobi">главная</a> | <a href=
+"http://www.pautinka.mobi/inform.php">информация</a> | <a href="http://www.pautinka.mobi/trening.php">торговля</a> | <a href=
+"http://www.pautinka.mobi">выход</a></td></tr>
 <tr><td colspan=2 align="center" valign="middle"><h3>Паутинка!</h3> символизирует порядок и равновесие сил, 
 однако основное значение символа,<br/> связывать и удерживать.</td></tr>
 <tr><td colspan=2>&nbsp;</td></tr>
 <tr><td colspan=2 align="center" valign="middle"><span style="color:gray;"><hr/></span>&copy; 2014 <a href=
-"https://www.pautinka.mobi/inform.php">pautinka.mobi</a></td></tr></table>
+"http://www.pautinka.mobi/inform.php">pautinka.mobi</a></td></tr></table>
 </body>
 </html>
