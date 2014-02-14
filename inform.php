26c9fbd42dc1461c4725bcd06e4d484447c4f7d4
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-14 21:51:43 +0300

    Update inform.php

--- inform.php
+++ inform.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ИНФОРМ ПАУТИНКА </title></head>
 <body style = "padding:0; margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td align="center" valign="middle"><a href="http://pautinka.mobi/main.php">pautinka.mobi</a></td>
+<tr><td align="center" valign="middle"><a href="http://pautinka.mobi">pautinka.mobi</a></td>
 <td align="center" valign="middle"><a href="http://pautinka.mobi/main.php">rus</a> | <a href=
 "http://pautinka.mobi/torg.php">eng</a></td></tr>
 <tr><td colspan=2><img width="100%" src= "pauk2-1-2.jpg"/></td></tr>
@@ -25,7 +25,7 @@ pautinka.mobi
 3 ГОЛОСА = 1 ПАУТИНКA</td></tr>
 <tr><td colspan=2>&nbsp</td></tr>
 <tr><td colspan=2 align="center" valign="middle"><span stule="color:gray;"><hr/></span>
-&copy; 2014 <a href="http://pautinka.mobi/main.php">pautinka.mobi</a></td></tr>
+&copy; 2014 <a href="http://pautinka.mobi">pautinka.mobi</a></td></tr>
 </table>
 </body>
 </html>
