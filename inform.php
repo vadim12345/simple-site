419661db0948db8a8a3127ee042c845157699a68
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-14 19:56:28 +0300

    Update inform.php

--- inform.php
+++ inform.php
@@ -3,16 +3,19 @@
 <head>
 <meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ИНФОРМ ПАУТИНКА </title></head>
 <body style = "padding:0; margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td align="center" valign="middle"><a href="pautinka.mobi">pautinka.mobi</a></td>
-<td align="center" valign="middle"><a href="https://pautinka.mobi/main.php">rus</a> | <a href=
-"pautinka.mobi/default1.php">eng</a></td></tr>
+<tr><td align="center" valign="middle"><a href="http://pautinka.mobi/main.php">pautinka.mobi</a></td>
+<td align="center" valign="middle"><a href="http://pautinka.mobi/main.php">rus</a> | <a href=
+"http://pautinka.mobi/torg.php">eng</a></td></tr>
 <tr><td colspan=2><img width="100%" src= "pauk2-1-2.jpg"/></td></tr>
-<tr><td colspan=2>&nbsp</td></tr>
+
+<tr><td colspan=2 align="center" valign="middle"><a href="http://pautinka.mobi/main.php">главная</a> | <a href=
+"http://pautinka.mobi/inform.php">информ</a> | <a href="http://pautina.mobi/torg.php">торговля</a> | <a href=
+"http://pautinka.mobi">выход</a></td></tr>
 <tr><td colspan=2 align="center" valign="middle">
 <h3>Как это работает?</h3>
 После регистрации Вы получите
 специальную реферальную ссылку.
-<a href="pautinka.mobi">https://pautinka.mobi</a>
+<a href="http://pautinka.mobi">http://pautinka.mobi</a>
 Разошлите её другим людям и как
 только они зарегистрируются на
 pautinka.mobi
@@ -22,7 +25,7 @@ pautinka.mobi
 3 ГОЛОСА = 1 ПАУТИНКA</td></tr>
 <tr><td colspan=2>&nbsp</td></tr>
 <tr><td colspan=2 align="center" valign="middle"><span stule="color:gray;"><hr/></span>
-&copy; 2014 <a href="https://pautinka.mobi/main.php">pautinka.mobi</a></td></tr>
+&copy; 2014 <a href="http://pautinka.mobi/main.php">pautinka.mobi</a></td></tr>
 </table>
 </body>
 </html>
