9d9d5a8053279b8ff51f50af935acb4663e610e4
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-14 20:27:19 +0300

    Update main.php

--- main.php
+++ main.php
@@ -10,7 +10,7 @@ eng</a></td></tr>
 <tr><td colspan=2><img width="100%" src= "pauk1-1.jpg"/></td></tr>
 
 <tr><td colspan=2 align="center" valig="midle"><a href="http://www.pautinka.mobi/main.php">главная</a> | <a href=
-"http://www.pautinka.mobi/inform.php">информ</a> | <a href="http://www.pautinka.mobi/trening.php">торговля</a> | <a href=
+"http://www.pautinka.mobi/inform.php">информ</a> | <a href="http://www.pautinka.mobi/torg.php">торговля</a> | <a href=
 "http://www.pautinka.mobi">выход</a></td></tr>
 <tr><td colspan=2 align="center" valign="middle"><h3><br/>Паутинка!</h3> символизирует порядок и равновесие сил, 
 однако основное значение символа,<br/> связывать и удерживать.</td></tr>
