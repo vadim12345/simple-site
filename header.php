913bf72cd01dd3e7f1e2633b0dd4f031a738f051
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-15 20:11:39 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,9 @@
-<?php
+<!DOCTOR HTML PUBLIC "-//w3c//System HTML 4.01 Transitional//End" "http://www.w3c.org/Tr/HTML/loose.dtd">
+<html>
+<head>
+<meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> <?php echo $title;?> </title></head>
+<body style = "padding:0; margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr><td align="center" valign="middle"><a href="http://pautinka.mobi">pautinka.mobi</a></td>
+<td align="center" valign="middle"><a href="http://pautinka.mobi/main.php">rus</a> | <a href=
+"http://pautinka.mobi/torg.php">eng</a></td></tr>
 
-?>
