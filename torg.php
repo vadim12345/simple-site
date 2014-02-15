1513ef233060f063e31be811fdad995aa29c6f7b
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-15 14:47:55 +0300

    Update torg.php

--- torg.php
+++ torg.php
@@ -1,28 +1,40 @@
+<!DOCTOR HTML PUBLIC "-//w3c//System HTML 4.01 Transitional//End" "http://www.w3c.org/Tr/HTML/loose.dtd">
 <html>
 <head>
-</head>
-<body><h6>БЕШАННАЯ ТОРГОВЛЯ</h6>
-<form name = "input" action= "form.php" method= "get" >
-Введите Ваше имя: <input type = "text" name ="name" />
-<input type = "submit" value = "Отправить" />
-</form>
+<meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ТОРГОВАЯ ПАУТИНКА </title></head>
+<body style = "padding:0; margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr><td align="center" valign="middle"><a href="http://pautinka.mobi">pautinka.mobi</a></td>
+<td align="center" valign="middle"><a href="http://pautinka.mobi/main.php">rus</a> | <a href=
+"http://pautinka.mobi/torg.php">eng</a></td></tr>
+<tr><td colspan=2><img width="100%" src= "z11.jpg"/></td></tr>
 
-<ul class= "menu" >
-<li><a href = "">Главная</a></li>
-<li><a href = "">О компании</a>
-<ul>
-<li><a href = "" >Производство</a></li>
-<li><a href = "" >Клиенты</a></li>
-<li><a href = "" >Для партнеров</a></li>
-<li><a href = "" >Для клиентов</a></li>
-<li><a href = "" >Реклама</a></li>
-</ul>
-</li>
-<li><a href = "">Услуги</a></li>
-<li><a href = "">Фотогалерея</a></li>
-<li><a href = "">Цены</a></li>
-<li><a href = "">Отзывы</a></li>
-<li><a href = "">Контакты</a></li>
-</ul>
+<tr><td colspan=2 align="center" valign="middle"><a href="http://pautinka.mobi/main.php">главная</a> | <a href=
+"http://pautinka.mobi/inform.php">информ</a> | <a href="http://pautinka.mobi/torg.php">торговля</a> | <a href=
+"http://pautinka.mobi">выход</a></td></tr>
+<tr><td colspan=2 align="center" valign="middle">
+<h3><br/>ТОРГОВЛЯ</h3>
+Торги проходят в электронном
+виде с использованием
+специализированных программ.<br/>
+Брокеры в своих интересах или
+интересах клиентов выставляют в
+торговые системы заявки на
+покупку или продажу. Эти
+заявки удовлетворяются
+встречными заявками других
+торговцев.<br/> ПАУТИНКА ведёт учёт
+исполненных сделок, реализует,
+организует и гарантирует
+расчёты (клиринг ), обеспечивает
+механизм взаимодействия
+«поставки против платежа»
+<br/>Реферальная ссылка: 
+<a href="http://pautinka.mobi">http://pautinka.mobi</a> - ЭТО КРУТО !!!<br/>
+</td></tr>
+<tr><td colspan=2>&nbsp</td></tr>
+<tr><td colspan=2 align="center" valign="middle"><span stule="color:gray;"><hr/></span>
+&copy; 2014 <a href="http://pautinka.mobi">pautinka.mobi</a></td></tr>
+</table>
 </body>
 </html>
+
