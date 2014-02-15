12e55c44da96cec4fbbafcda3fa6b89cba3de6b0
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-15 17:00:42 +0300

    Update default.php

--- default.php
+++ default.php
@@ -9,7 +9,7 @@
 <tr><td colspan="2" align="center" valign="middle"><img width="150" src="pauk.png"/></td></tr>
 <tr><td colspan="2" style="border:1px solid black; background-color:#FFFACD;" align="center" valign="middle">
 <table border="0" cellpadding="0" cellspacing="5">
-<form name = "input" action ="form.php" method= "get"></td><br/>&nbsp;<td align="right" valign="middle">E-mail:</td>
+<form name = "input" action ="form.php" method= "get"></td><td align="right" valign="middle">E-mail:</td>
 <td><input type = "text"/size=16></td></tr><tr><td align="right" valign="middle">Password:</td>
 <td><input type = "password"/size=16></td></tr>
 <tr><td colspan="2" align="center" valign="middle"><input type ="submit" style="color:BC8F8F" value="РЕГИСТРАЦИЯ / ВХОД"/><br/>&nbsp;</td></form></tr></table>
