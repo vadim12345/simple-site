77e5343543311d9ffb5e583b53cd2c02bb37c22f
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-16 00:05:20 +0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@
 <tr><td align="center" valign="middle"><a href="http://www.pautinka.mobi">pautinka.mobi</a></td>
 <td align="center" valign="middle"><a href="http://www.pautinka.mobi/main.php">rus</a> | <a href=
 "http://www.pautinka.mobi/torg.php">eng</a></td></tr>
-<tr><td colspan=2><img width="100%" src= "<?php echo $img;?"/></td></tr>
+<tr><td colspan=2><img width="100%" src= "<?php echo $img;?>"/></td></tr>
 
 <tr><td colspan=2 align="center" valig="midle"><a href="http://www.pautinka.mobi/main.php">главная</a> | <a href=
 "http://www.pautinka.mobi/inform.php">информ</a> | <a href="http://www.pautinka.mobi/torg.php">торговля</a> | <a href=
