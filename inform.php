d2aa34b548f4185e7b868759891469273f464786
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-16 15:49:20 +0300

    Update inform.php

--- inform.php
+++ inform.php
@@ -1,16 +1,5 @@
-<!DOCTOR HTML PUBLIC "-//w3c//System HTML 4.01 Transitional//End" "http://www.w3c.org/Tr/HTML/loose.dtd">
-<html>
-<head>
-<meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ИНФОРМ ПАУТИНКА </title></head>
-<body style = "padding:0; margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td align="center" valign="middle"><a href="http://pautinka.mobi">pautinka.mobi</a></td>
-<td align="center" valign="middle"><a href="http://pautinka.mobi/main.php">rus</a> | <a href=
-"http://pautinka.mobi/torg.php">eng</a></td></tr>
-<tr><td colspan=2><img width="100%" src= "pauk2-1-2.jpg"/></td></tr>
-
-<tr><td colspan=2 align="center" valign="middle"><a href="http://pautinka.mobi/main.php">главная</a> | <a href=
-"http://pautinka.mobi/inform.php">информ</a> | <a href="http://pautinka.mobi/torg.php">торговля</a> | <a href=
-"http://pautinka.mobi">выход</a></td></tr>
+<?php $title=" ИНФОРМ ПАУТИНКА ";require_once("header.php");?>
+<?php $img="pauk2-1-2.jpg";require_once("menu.php");?>
 <tr><td colspan=2 align="center" valign="middle">
 <h3><br/>Как это работает?</h3>
 После регистрации Вы получите
@@ -23,9 +12,5 @@ pautinka.mobi
 Чем больше людей Вы пригласите, 
 тем больше ГОЛОСОВ получите!<br/>
 3 ГОЛОСА = 1 ПАУТИНКA</td></tr>
-<tr><td colspan=2>&nbsp</td></tr>
-<tr><td colspan=2 align="center" valign="middle"><span stule="color:gray;"><hr/></span>
-&copy; 2014 <a href="http://pautinka.mobi">pautinka.mobi</a></td></tr>
-</table>
-</body>
-</html>
+<?php require_once("footer.php");?>
+
