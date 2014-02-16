85e504b2841c454e08122a4903c095df38f33351
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-16 20:29:27 +0300

    Update default.php

--- default.php
+++ default.php
@@ -1,7 +1,4 @@
-<!DOCTOR HTML PUBLIC "-//w3c//System HTML 4.01 Transitional//End" "http://www.w3c.org/Tr/HTML/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/><title> МОБИЛЬНАЯ ПАУТИНКА </title></head>
+<?php $title=" МОБИЛЬНАЯ ПАУТИНКА ";require_once("header.php");?>
 
 <body style="padding:0;margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><a href="http://pautinka.mobi">pautinka.mobi</a></td><td align="center" valign="middle">
@@ -13,7 +10,6 @@
 <td><input type = "text"/size=16></td></tr><tr><td align="right" valign="middle">Password:</td>
 <td><input type = "password"/size=16></td></tr>
 <tr><td align="center" valign="middle">&nbsp</td><td><input type ="submit" value= "ВХОД"/></td></form></tr></table>
-<tr><td colspan="2" style="background-color:#E6E6FA" align="center" valign="middle"><br/> &copy; 2014 <a href="http://pautinka.mobi">pautinka.mobi</a>
-</td></tr></table>
-</body>
-</html>
+
+<tr><td colspan=2 align="center" valign="middle">&nbsp</td></tr>
+<?php require_once("footer.php");?>
