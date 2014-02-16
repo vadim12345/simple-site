d5b9304468613f73170a3585dfcd786e1b9bc8f0
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-16 16:14:17 +0300

    Update torg.php

--- torg.php
+++ torg.php
@@ -1,16 +1,5 @@
-<!DOCTOR HTML PUBLIC "-//w3c//System HTML 4.01 Transitional//End" "http://www.w3c.org/Tr/HTML/loose.dtd">
-<html>
-<head>
-<meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ТОРГОВАЯ ПАУТИНКА </title></head>
-<body style = "padding:0; margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td align="center" valign="middle"><a href="http://pautinka.mobi">pautinka.mobi</a></td>
-<td align="center" valign="middle"><a href="http://pautinka.mobi/main.php">rus</a> | <a href=
-"http://pautinka.mobi/torg.php">eng</a></td></tr>
-<tr><td colspan=2><img width="100%" src= "z11.jpg"/></td></tr>
-
-<tr><td colspan=2 align="center" valign="middle"><a href="http://pautinka.mobi/main.php">главная</a> | <a href=
-"http://pautinka.mobi/inform.php">информ</a> | <a href="http://pautinka.mobi/torg.php">торговля</a> | <a href=
-"http://pautinka.mobi">выход</a></td></tr>
+<?php $title=" ТОРГОВАЯ ПАУТИНКА ";require_once("header.php");?>
+<?php $img="z11.jpg";require_once("menu.php");?>
 <tr><td colspan=2 align="center" valign="middle">
 <h3><br/>ТОРГОВЛЯ</h3>
 Торги проходят в электронном
@@ -31,10 +20,5 @@
 <br/>Реферальная ссылка:<br/> 
 <a href="http://pautinka.mobi">http://pautinka.mobi</a>ЭТО КРУТО !!!<br/>
 </td></tr>
-<tr><td colspan=2>&nbsp</td></tr>
-<tr><td colspan=2 align="center" valign="middle"><span stule="color:gray;"><hr/></span>
-&copy; 2014 <a href="http://pautinka.mobi">pautinka.mobi</a></td></tr>
-</table>
-</body>
-</html>
-
+<tr><td colspan=2 align="center" valign="middle">&nbsp</td></tr>
+<?php require_once("footer.php");?>
