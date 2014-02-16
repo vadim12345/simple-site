a4df66487057ac442e101dd46b830ee95aff899f
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-16 21:38:30 +0300

    Update default.php

--- default.php
+++ default.php
@@ -4,11 +4,12 @@
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><a href="http://pautinka.mobi">pautinka.mobi</a></td><td align="center" valign="middle">
 <a href="http://pautinka.mobi/main.php">rus</a> | <a href="http://pautinka.mobi/torg.php">eng</a></td></tr>
 <tr><td colspan="2" align="center" valign="middle"><img width="150" src="pauk.png"/></td></tr>
+
 <tr><td colspan="2" style="border:1px solid black; background-color:#FFFACD;" align="center" valign="middle">
 <table border="0" cellpadding="0" cellspacing="5">
 <form name = "input" action ="http://pautinka.mobi/main.php" method= "get"></td><td align="right" valign="middle">E-mail:</td>
-<td><input type = "text"/size=16></td></tr><tr><td align="right" valign="middle">Password:</td>
-<td><input type = "password"/size=16></td></tr>
+<td><input type = "text"/size=16 name="e-mail"/></td></tr><tr><td align="right" valign="middle">Password:</td>
+<td><input type = "password"/size=16 namename="passvord"/></td></tr>
 <tr><td align="center" valign="middle">&nbsp</td><td><input type ="submit" value= "ВХОД"/></td></form></tr></table>
 
 <tr><td colspan=2 align="center" valign="middle">&nbsp</td></tr>
