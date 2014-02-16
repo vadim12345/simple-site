809d9b1f5651e82599c3cd99d5551f5f6ceac8a1
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-16 15:33:45 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,8 +1,9 @@
 <?php $title="ГЛАВНАЯ ПАУТИНКА";require_once("header.php");?>
 <?php $img="pauk1-1.jpg";require_once("menu.php");?>
-<?php require_once("footer.php");?>
-
 <tr><td colspan=2 align="center" valign="middle"><h3><br/>Паутинка!</h3> символизирует порядок и равновесие сил, 
 однако основное значение символа,<br/> связывать и удерживать.</td></tr>
+<?php require_once("footer.php");?>
+
+
 
 
