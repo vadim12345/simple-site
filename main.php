8027b84ce27152a6ea3b6502996e75a884fdb5b2
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-16 23:25:23 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php $title="ГЛАВНАЯ ПАУТИНКА";require_once("header.php");?>
 <?php $img="pauk1-1.jpg";require_once("menu.php");?>
-<?php echo "E-mail:".$_POST["e-mail"]." ";echo "Password:".$_POST["password"];?>
+<tr><td><?php echo "E-mail:<strong>".$_POST["e-mail"]."</strong><br/>";echo "Password:".$_POST["password"];?></td</tr>
 
 <tr><td colspan=2 align="center" valign="middle"><h3><br/>Паутинка!</h3> символизирует порядок и равновесие сил, 
 однако основное значение символа,<br/> связывать и удерживать.</td></tr>
