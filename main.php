9c6faa70efcefac449fcf907c9cdb15ac3a4e766
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-16 15:29:37 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,21 +1,8 @@
-<!DOCTOR HTML PUBLIC "-//w3c//System HTML 4.01 Transitional//End" "http://www.w3c.org/Tr/HTML/loose.dtd">
-<html>
-<head>
-<meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/><title> ГЛАВНАЯ ПАУТИНКА </title></head>
-<body style="padding: 0; margin:0;">
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td align="center" valign="middle"><a href="http://www.pautinka.mobi">pautinka.mobi</a></td>
-<td align="center" valign="middle"><a href="http://www.pautinka.mobi/main.php">rus</a> | <a href=
-"http://www.pautinka.mobi/torg.php">eng</a></td></tr>
-<tr><td colspan=2><img width="100%" src= "pauk1-1.jpg"/></td></tr>
+<?php $title="ГЛАВНАЯ ПАУТИНКА";require_once("header.php");?>
+<?php $img="pauk1-1.jpg";require_once("menu.php");?>
+<?php require_once("footer.php");?>
 
-<tr><td colspan=2 align="center" valig="midle"><a href="http://www.pautinka.mobi/main.php">главная</a> | <a href=
-"http://www.pautinka.mobi/inform.php">информ</a> | <a href="http://www.pautinka.mobi/torg.php">торговля</a> | <a href=
-"http://www.pautinka.mobi">выход</a></td></tr>
 <tr><td colspan=2 align="center" valign="middle"><h3><br/>Паутинка!</h3> символизирует порядок и равновесие сил, 
 однако основное значение символа,<br/> связывать и удерживать.</td></tr>
-<tr><td colspan=2>&nbsp;</td></tr>
-<tr><td colspan=2 align="center" valign="middle"><span style="color:gray;"><hr/></span>&copy; 2014 <a href=
-"http://www.pautinka.mobi">pautinka.mobi</a></td></tr></table>
-</body>
-</html>
+
+
