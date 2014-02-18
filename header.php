31d66498a0ee2bc29f3b059e5813c5ba176c1b62
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-18 18:34:16 +0300

    Update header.php

--- header.php
+++ header.php
@@ -3,5 +3,6 @@
 <head>
 <meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8"/>
 <title> <?php echo $title;?></title>
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
