84bc1d6876c02fc1c63194b0a44bce5a2a9388c4
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-20 20:22:18 +0300

    Update torg.php

--- torg.php
+++ torg.php
@@ -1,7 +1,7 @@
 <?php $title=" ТОРГОВАЯ ПАУТИНКА ";require_once("header.php");?>
 <?php $img="z17.jpg";require_once("body.php");?>
-<table border="0"><tr><td colspan="2">
-<table border="1" width="100%" align="center" valign="middle" cellpadding="5" cellspacing="2">
+<table border="0" align="center" valign="middle"><tr><td colspan="2">
+<table border="1" width="100%" align="center" valign="middle" cellpadding="3" cellspacing="0">
 <tr><td align="center">PAU</td><td align="center">USD</td>
 <td align="center">EUR</td><td align="center">BTC</td><td align="center">RUR</td></tr>
 <tr><td align="center"><img width="30" height="30" src="w1.jpg"></td><td align="center">3.3</td><td align="center">2.64</td><td align="center">0.0055</td><td align="center">132</td></tr>
