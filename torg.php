b560b8658bdf1dabef5575b2b4e890dc01ab1bee
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-20 19:03:42 +0300

    Update torg.php

--- torg.php
+++ torg.php
@@ -1,20 +1,14 @@
 <?php $title=" ТОРГОВАЯ ПАУТИНКА ";require_once("header.php");?>
 <?php $img="z17.jpg";require_once("body.php");?>
 
-<table border="1" width="100%" align="center" cellpadding="5" cellspacing="2">
+<table border="1" width="100%" align="center" valign="middle" cellpadding="5" cellspacing="2">
 <tr><td align="center">PAU</td><td align="center">USD</td>
 <td align="center">EUR</td><td align="center">BTC</td><td align="center">RUR</td></tr>
-<tr><td align="center"><img width="30" height="30" src="w1.jpg"></td><td align="center">3.3</td><td align="center">
-2.64</td><td align="center">0.0055</td><td align="center">132</td></tr>
-<tr><td align="center"><img width="30" height="30" src="w2.jpg"></td><td align="center">33</td><td align="center">
-26.4</td><td align="center">0.055</td>
-<td align="center">1.320</td></tr>
-<tr><td align="center"><img width="30" height="30" src="w3.jpg"></td><td align="center">330</td><td align="center">
-264</td><td align="center">0.55</td><td align="center">13.200</td></tr>
-<tr><td align="center"><img width="30" height="30" src="w4.jpg"></td><td align="center">3.300</td><td align="center">
-2.640</td><td align="center">5.5</td><td align="center">132.000</td></tr>
-<tr><td align="center"><img width="30" height="30" src="w5.jpg"></td><td align="center">33.000</td><td align="center">
-26.400</td><td align="center">55</td><td align="center">1.320.000</td></tr>
+<tr><td align="center"><img width="30" height="30" src="w1.jpg"></td><td align="center">3.3</td><td align="center">2.64</td><td align="center">0.0055</td><td align="center">132</td></tr>
+<tr><td align="center"><img width="30" height="30" src="w2.jpg"></td><td align="center">33</td><td align="center">26.4</td><td align="center">0.055</td><td align="center">1.320</td></tr>
+<tr><td align="center"><img width="30" height="30" src="w3.jpg"></td><td align="center">330</td><td align="center">264</td><td align="center">0.55</td><td align="center">13.200</td></tr>
+<tr><td align="center"><img width="30" height="30" src="w4.jpg"></td><td align="center">3.300</td><td align="center">2.640</td><td align="center">5.5</td><td align="center">132.000</td></tr>
+<tr><td align="center"><img width="30" height="30" src="w5.jpg"></td><td align="center">33.000</td><td align="center">26.400</td><td align="center">55</td><td align="center">1.320.000</td></tr>
 </table>
 
 
@@ -27,6 +21,5 @@
 исполненных сделок, реализует,
 организует и гарантирует
 расчёты.</td></tr>
-
 <tr><td align="center" valign="middle">&nbsp</td></tr>
 <?php require_once("footer.php");?>
