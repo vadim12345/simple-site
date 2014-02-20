36ce08797f25f42bd252f72c1373db9ce0b08160
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-21 00:24:42 +0300

    Update body.php

--- body.php
+++ body.php
@@ -5,7 +5,7 @@ pautinka.mobi</a></td><td align="center" valign="middle"><a href="http://www.pau
 "http://www.pautinka.mobi/torg.php">eng</a></td></tr>
 <tr><td colspan=2><img width="100%" src= "<?php echo $img;?>"/></td></tr>
 
-<tr><td colspan=2 style="background-color:#000000;" align="center" valign="middle"><a href=
+<tr><td colspan=2 style="background-color:#E6E6FA;" align="center" valign="middle"><a href=
 "http://www.pautinka.mobi/main.php">главная</a> | <a href=
 "http://www.pautinka.mobi/inform.php">информ</a> | <a href="http://www.pautinka.mobi/torg.php">торговля</a> | <a href=
 "http://www.pautinka.mobi/game.php">игра</a> | <a href="http://www.pautinka.mobi/dnevnik.php">дневник</a> | <a href=
