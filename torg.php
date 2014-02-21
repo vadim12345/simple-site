44640185bedc191fe66f20b0a07c985b1fac2cec
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-21 13:58:45 +0300

    Update torg.php

--- torg.php
+++ torg.php
@@ -1,18 +1,18 @@
 <?php $title=" ТОРГОВАЯ ПАУТИНКА ";require_once("header.php");?>
 <?php $img="z17.jpg";require_once("body.php");?>
 
-<table border="1" width="100%" align="center" valign="middle" cellpadding="0" cellspacing="2">
-<tr><td align="center">PAU</td><td align="center">USD</td>
-<td align="center">BTC</td><td align="center">RUR</td></tr>
-<tr><td align="center"><img width="30" height="30" src="w1.jpg"></td><td align="center">3.3</td>
+<table border="1" width="100%" align="center" valign="middle" cellpadding="2" cellspacing="2">
+<tr><td align="center" width="40">PAU</td><td align="center" width="40">USD</td>
+<td align="center" width="40">BTC</td><td align="center" width="40">RUR</td></tr>
+<tr><td align="center"><img width="40" height="40" src="w1.jpg"></td><td align="center">3.3</td>
 <td align="center">0.0055</td><td align="center">132</td></tr>
-<tr><td align="center"><img width="30" height="30" src="w2.jpg"></td><td align="center">33</td>
+<tr><td align="center"><img width="40" height="40" src="w2.jpg"></td><td align="center">33</td>
 <td align="center">0.055</td><td align="center">1.320</td></tr>
-<tr><td align="center"><img width="30" height="30" src="w3.jpg"></td><td align="center">330</td>
+<tr><td align="center"><img width="40" height="40" src="w3.jpg"></td><td align="center">330</td>
 <td align="center">0.55</td><td align="center">13.200</td></tr>
-<tr><td align="center"><img width="30" height="30" src="w4.jpg"></td><td align="center">3.300</td>
+<tr><td align="center"><img width="40" height="40" src="w4.jpg"></td><td align="center">3.300</td>
 <td align="center">5.5</td><td align="center">132.000</td></tr>
-<tr><td align="center"><img width="30" height="30" src="w5.jpg"></td><td align="center">33.000</td>
+<tr><td align="center"><img width="40" height="40" src="w5.jpg"></td><td align="center">33.000</td>
 <td align="center">55</td><td align="center">1.320.000</td></tr>
 </table>
 
