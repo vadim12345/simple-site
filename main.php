806603295fa6e4f913bd76a70bc94732bbf82036
vadim12345 <vadim12345@users.noreply.github.com>
2014-02-21 15:08:02 +0300

    Update main.php

--- main.php
+++ main.php
@@ -2,11 +2,16 @@
 <?php $img="pauk1-1.jpg";require_once("body.php");?>
 <tr><td><?php echo "<i>Добро пожаловать!</i><br/>E-mail:<strong>".$_POST["e-mail"]."</strong><br/>";
 echo "Password: ".$_POST["password"];?></td</tr>
+
 <table border="1" width="100%" align="center" valign="middle" cellpadding="2" cellspacing="2">
 <tr><td rowspan="3" align="center" valign="middle" width="120"><img width="120" height="120" src="pauk3.jpg"></td>
 <td style="border:0;" colspan="5" align="left" valign="middle">name: Vadim12345</td></tr>
-<tr><td align="center" valign="middle">PAU</td>
-
+<tr><td align="center" valign="middle">PAU</td><td align="center" valign="middle">PAU</td><td align
+="center" valign="middle">PAU</td>
+<td align="center" valign="middle">PAU</td><td align="center" valign="middle">PAU</td></tr>
+<tr><td align="center" valign="middle">0</td><td align="center" valign="middle">0</td><td align
+="center" valign="middle">0</td><td align="center" valign="middle">0</td><td align="center" valign="middle">0</td></tr>
+</table>
 
 <tr><td colspan=2 align="center" valign="middle"><h3><br/>Паутинка!</h3> символизирует порядок и равновесие сил, 
 однако основное значение символа,<br/> связывать и удерживать.</td></tr>
