3e0e2f708ee7ab84f05efd5b19f0c6bd200f5322
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-01 23:50:41 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,7 +1,10 @@
 <?php $title="ГЛАВНАЯ ПАУТИНКА";require_once("header.php");?>
 <?php $img="x2.jpg";require_once("body.php");?>
-<tr><td><?php echo "<i>Добро пожаловать!</i><br/>E-mail:<strong>".$_POST["e-mail"]."</strong><br/>";
-echo "Password: ".$_POST["password"];?></td</tr>
+
+<table><tr><td><?php echo "<i>Добро пожаловать!</i><br/>E-mail:<strong>".$_POST["e-mail"]."</strong><br/>";
+echo "Password: ".$_POST["password"];?></td</tr></table>
+
+<table><tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr></table>
 
 <table border="1" colspan="4" width="100%" align="center" valign="middle" cellpadding="2" cellspacing="2">
 <tr><td style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"> <img width="100%" height="100%" src="pauk3.jpg"></td><td style="border:0; color:#2F4F4F;" colspan="2" align="left" valign="middle"> 
@@ -72,21 +75,10 @@ align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p><
 <td align="center" valign="middle">0</td></tr>
 </table>
 
-<table style="border:0;" colspan="4" width="100%" align="center" valign="middle" cellpadding="2"
-cellspacing="2">
-<tr><td align="center" valign="middle">&nbsp</td></tr>
-<tr><td colspan="4" align="center" valign="middle">
-<p style="font-family:Verdana; font-size:12px"><a href="http://cbrf.magazinfo.ru/">Курс обмена валют (CHF/RUR):</a><br><a href="http://cbrf.magazinfo.ru/rur/CHF"><script>
-var cbrfmag= new Date();
-document.write('<img src="http://cbrf.magazinfo.ru/informer1/CHF_RUR.png?'+cbrfmag.getDate()+'" border="0" alt="График для прогноза курсов обмена валют (данные ЦБ РФ): Швейцарского франка к рублю" width="200" height="150" vspace="5">');</script><noscript><img src="http://cbrf.magazinfo.ru/informer1/CHF_RUR.png" border="0" alt="Курсы Швейцарского франка к рублю - курсы обмена валют ЦБ РФ" width="200" height="150" vspace="5"></noscript></a></p></td></tr>
-</table></td></tr>
-</table>
-
-
-<tr><td colspan="4" align="center" valign="middle"><h3><br/>Паутинка!</h3> символизирует порядок и равновесие сил, 
-однако основное значение символа,<br/> связывать и удерживать.</td></tr>
-
-<tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr>
+<table><tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr></table>
+<table><tr><td colspan="4" align="center" valign="middle"><h3><br/>Паутинка!</h3> символизирует порядок и равновесие сил, 
+однако основное значение символа,<br/> связывать и удерживать.</td></tr></table>
+<table><tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr></table>
 <?php require_once("footer.php");?>
 
 
