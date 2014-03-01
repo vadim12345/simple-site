e01b6e8a660c723be94098674410fe6887298246
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-02 00:50:08 +0300

    Update default.php

--- default.php
+++ default.php
@@ -1,9 +1,23 @@
 <?php $title=" МОБИЛЬНАЯ ПАУТИНКА ";require_once("header.php");?>
 
-<body style="padding:0;margin:0;"><table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><a href="http://pautinka.mobi">pautinka.mobi</a></td><td align="center" valign="middle">
+<body style="padding:0;margin:0;">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><a href="http://pautinka.mobi">pautinka.mobi
+</a></td><td align="center" valign="middle">
 <a href="http://pautinka.mobi/main.php">rus</a> | <a href="http://pautinka.mobi/torg.php">eng</a></td></tr>
-<tr><td colspan="2" align="center" valign="middle"><img width="150" src="pauk.png"/></td></tr>
+
+
+<table border="0" width="100%" align="center" valign="middle" cellpadding="3" cellspacing="2">
+<tr><td style="border:0;" rowspan="6" width="10%" align="center" valign="middle"><p>&nbsp</p></td>
+<td style="border:0;" width="25%" align="center" valign="middle"><p>&nbsp</p></td><td style="border:0;" 
+rowspan="6" width="5%" align="center" valign="middle"><p>&nbsp</p></td><td style="border:0;" rowspan="6"
+width="60%" align="center" valign="middle"><img width="100%" src="pauk.png"/></td></tr><tr><td style="border:0;
+background-color:#DC143C;" align="center" valign="middle"><p style="color:white">PAU tina</p></td></tr><tr>
+<td style="border:0; background-color:#6A5ACD;" align="center" valign="middle"><p style="color:white">PAU chok</p></td>
+</tr><tr><td style="border:0; background-color:#9ACD32;" align="center" valign="middle"><p style="color:white">PAU coco
+</p></td></tr><tr><td style="border:0; background-color:gold;" align="center" valign="middle"><p style="color:white">
+PAU gold</p></td></tr> <tr><td align="center" valign="middle"><p>&nbsp</p></td></tr>
+</table>
 
 <tr><td colspan="2" style="border:0; background-color:#FFFACD;" align="center" valign="middle">
 <table border="0" cellpadding="0" cellspacing="7">
@@ -11,7 +25,8 @@
 <td><input type="text"/size=16 name="e-mail"/></td></tr>
 <tr><td align="right" valign="middle">Password:</td>
 <td><input type="password"/size=16 name="password"/></td></tr>
-<tr><td align="center" valign="middle">&nbsp</td><td><input type="submit" name="submit" value="ВХОД"/></td></form></tr></table>
+<tr><td align="center" valign="middle">&nbsp</td><td><input type="submit" name="submit" value="ВХОД"/></td></form>
+</tr></table>
 
 <tr><td colspan="2" align="center" valign="middle">&nbsp</td></tr>
 <?php require_once("footer.php");?>
