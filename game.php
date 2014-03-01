048ba8ee8ac1b98da97fe5d52199f2a289b7da14
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-01 23:13:01 +0300

    Update game.php

--- game.php
+++ game.php
@@ -1,6 +1,9 @@
 <?php $title=" ИГРОВАЯ ПАУТИНКА ";require_once("header.php");?>
-<?php $img="z14-2.jpg";require_once("body.php");?>
-<tr><td colspan=2 align="center" valign="middle"><b>игра</b><br/>вид осмысленной
+<?php $img="x2.jpg";require_once("body.php");?>
+
+<table><tr><td colspan="2" align="center" valign="middle">&nbsp</td></tr></table>
+
+<tr><td colspan="2" align="center" valign="middle"><b>игра</b><br/>вид осмысленной
 непродуктивной деятельности,
 где мотив лежит не в результате
 её, а в самом процессе. Также
@@ -8,5 +11,6 @@
 обозначения набора предметов
 или программ , предназначенных
 для подобной деятельности.</td></tr>
-<tr><td colspan=2 align="center" valign="middle">&nbsp</td></tr>
+<tr><td colspan="2" align="center" valign="middle">&nbsp</td></tr>
+
 <?php require_once("footer.php");?>
