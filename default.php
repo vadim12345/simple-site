d3d7a997846119e4bf30dcecb004ee87de978395
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-02 01:00:24 +0300

    Update default.php

--- default.php
+++ default.php
@@ -1,10 +1,11 @@
 <?php $title=" МОБИЛЬНАЯ ПАУТИНКА ";require_once("header.php");?>
 
 <body style="padding:0;margin:0;">
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><a href="http://pautinka.mobi">pautinka.mobi
+<table width="100%" border="0" cellpadding="0" cellspacing="0"><tr><td>
+
+<table><tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><a href="http://pautinka.mobi">pautinka.mobi
 </a></td><td align="center" valign="middle">
-<a href="http://pautinka.mobi/main.php">rus</a> | <a href="http://pautinka.mobi/torg.php">eng</a></td></tr>
+<a href="http://pautinka.mobi/main.php">rus</a> | <a href="http://pautinka.mobi/torg.php">eng</a></td></tr></table>
 
 
 <table border="0" width="100%" align="center" valign="middle" cellpadding="3" cellspacing="2">
@@ -19,7 +20,7 @@ background-color:#DC143C;" align="center" valign="middle"><p style="color:white"
 PAU gold</p></td></tr> <tr><td align="center" valign="middle"><p>&nbsp</p></td></tr>
 </table>
 
-<tr><td colspan="2" style="border:0; background-color:#FFFACD;" align="center" valign="middle">
+<tr><td colspan="4" style="border:0; background-color:#FFFACD;" align="center" valign="middle">
 <table border="0" cellpadding="0" cellspacing="7">
 <form action="http://pautinka.mobi/main.php" method="post"></td><td align="right" valign="middle">E-mail:</td>
 <td><input type="text"/size=16 name="e-mail"/></td></tr>
@@ -28,5 +29,6 @@ PAU gold</p></td></tr> <tr><td align="center" valign="middle"><p>&nbsp</p></td><
 <tr><td align="center" valign="middle">&nbsp</td><td><input type="submit" name="submit" value="ВХОД"/></td></form>
 </tr></table>
 
-<tr><td colspan="2" align="center" valign="middle">&nbsp</td></tr>
+<table><tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr></table>
+
 <?php require_once("footer.php");?>
