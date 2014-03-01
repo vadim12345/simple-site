d8077de06d2f7ac324f536fda74d6628d7a4e785
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-01 22:05:52 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 </td></tr>
-<tr><td colspan="2" style="background-color:#E6E6FA" align="center" valign="middle"> &copy; 2014 <a href=
+<tr><td colspan="4" style="background-color:#E6E6FA;" align="center" valign="middle"> &copy; 2014 <a href=
 "http://www.pautinka.mobi">pautinka.mobi</a></td></tr></table>
 </body>
 </html>
