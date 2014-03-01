eff6355fc30c0feec7ae5901f1acc79e7c65c95a
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-01 22:17:29 +0300

    Update body.php

--- body.php
+++ body.php
@@ -1,8 +1,11 @@
 <body style="padding:0; margin:0;">
 <table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><a href="http://www.pautinka.mobi">
+
+<tr><td colspan="4" style="background-color:#E6E6FA;" align="center" valign="middle"><a href="http://www.pautinka.mobi">
 pautinka.mobi</a></td><td align="center" valign="middle"><a href="http://www.pautinka.mobi/main.php">rus</a> | <a href=
 "http://www.pautinka.mobi/torg.php">eng</a></td></tr>
+
+
 <tr><td colspan="4"><img width="100%" src= "<?php echo $img;?>"/></td></tr>
 
 <tr><td colspan="4" style="background-color:#E6E6FA;" align="center" valign="middle"><a href=
