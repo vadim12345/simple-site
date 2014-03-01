7b322268b22b3f114dfaf66bf93740ae259ea9de
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-01 20:04:52 +0300

    Update default.php

--- default.php
+++ default.php
@@ -6,7 +6,7 @@
 <tr><td colspan="2" align="center" valign="middle"><img width="150" src="pauk.png"/></td></tr>
 
 <tr><td colspan="2" style="border:0; background-color:#FFFACD;" align="center" valign="middle">
-<table border="0" cellpadding="0" cellspacing="5">
+<table border="0" cellpadding="0" cellspacing="7">
 <form action="http://pautinka.mobi/main.php" method="post"></td><td align="right" valign="middle">E-mail:</td>
 <td><input type="text"/size=16 name="e-mail"/></td></tr>
 <tr><td align="right" valign="middle">Password:</td>
