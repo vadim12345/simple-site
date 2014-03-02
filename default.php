d7d3559a70020c88785a930548ab019e3e862c21
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-02 13:50:50 +0300

    Update default.php

--- default.php
+++ default.php
@@ -10,8 +10,8 @@
 href="http://pautinka.mobi/torg.php">eng</a></td></tr>
 
 <table border="0" width="100%" align="center" valign="middle" cellpadding="3" cellspacing="2">
-<tr><td style="border:0;" rowspan="6" width="10%" align="center" valign="middle"><p>&nbsp</p></td>
-<td style="border:0;" width="25%" align="center" valign="middle"><p>&nbsp</p></td>
+<tr><td style="border:0;" rowspan="6" width="5%" align="center" valign="middle"><p>&nbsp</p></td>
+<td style="border:0;" width="30%" align="center" valign="middle"><p>&nbsp</p></td>
 <td style="border:0;" rowspan="6" width="5%" align="center" valign="middle"><p>&nbsp</p></td>
 <td style="border:0;" rowspan="6" width="60%" align="center" valign="middle"><img width="100%" src="pauk.png"/></td>
 </tr><tr><td style="border:0; background-color:#DC143C;" align="center" valign="middle">
@@ -22,9 +22,9 @@ align="center" valign="middle"><p style="color:white">PAU coco</p></td></tr>
 </td></tr><tr><td align="center" valign="middle"><p>&nbsp</p></td></tr>
 </table>
 
-<table border="0" width="100%" align="center" valign="middle" cellpadding="1" cellspacing="1">
+<table border="0" width="100%" align="center" valign="middle" cellpadding="0" cellspacing="0">
 <tr><td colspan="4" style="border:0; background-color:#FFFACD;" width="100%" align="center" valign="middle">
-<table border="0" cellpadding="1" cellspacing="7">
+<table border="0" cellpadding="0" cellspacing="5">
 <form action="http://pautinka.mobi/main.php" method="post"></td><td align="right" valign="middle">E-mail:</td>
 <td><input type="text"/size=16 name="e-mail"/></td></tr>
 <tr><td align="right" valign="middle">Password:</td>
