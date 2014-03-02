6efad61e3f29d433c8e4afcba35d8740078ec69a
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-02 17:58:57 +0300

    Update body.php

--- body.php
+++ body.php
@@ -1,14 +1,14 @@
 <body style="padding:0; margin:0;">
 <table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
 
-<tr><td style="background-color:#C0C0C0;" align="center" valign="middle"><a href="http://www.pautinka.mobi">
+<tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><a href="http://www.pautinka.mobi">
 pautinka.mobi</a></td><td align="center" valign="middle"><a href="http://www.pautinka.mobi/main.php">rus</a> | <a href=
 "http://www.pautinka.mobi/torg.php">eng</a></td></tr>
 
 
 <tr><td colspan="4"><img width="100%" src= "<?php echo $img;?>"/></td></tr>
 
-<tr><td colspan="4" style="background-color:#B0C4DE; text-decoration:none; color:white;"
+<tr><td colspan="4" style="background-color:#C0C0C0; text-decoration:none; color:white;"
 align="center" valign="middle"><a style="text-decoration:none; color:white;" href="http://www.pautinka.mobi/main.php">
 главная</a> | <a style="text-decoration:none; color:white;" href="http://www.pautinka.mobi/inform.php">информ</a> | <a 
 style="text-decoration:none; color:white;" href="http://www.pautinka.mobi/torg.php">торговля</a> | <a 
