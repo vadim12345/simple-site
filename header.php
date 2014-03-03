2d70dd84d1c6244cbb4cb2a0dd49ded6a94c258a
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-03 18:22:54 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//w3c//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTOR HTML PUBLIC "-//w3c//System HTML 4.01 Transitional//End" "http://www.w3c.org/Tr/HTML/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
