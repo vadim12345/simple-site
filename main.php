5065b03c4be34b2dcad4df1aa80f2c863f82c624
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-03 16:17:13 +0300

    Update main.php

--- main.php
+++ main.php
@@ -7,36 +7,32 @@ echo "Password: ".$_POST["password"];?></td</tr></table>
 <table><tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr></table>
 
 <table border="1" colspan="4" width="100%" align="center" valign="middle" cellpadding="2" cellspacing="2">
-<tr><td style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"> <img width="100%" height="100%" 
-src="p1.jpeg"></td><td style="border:0; color:#2F4F4F;" colspan="2" align="left" valign="middle"> 
-1.  Zaitseva</td></tr><tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
-<tr><td style="background-color:#DC143C;"
-align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p></td>
+<tr><td style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"><a border="0" href="p1.jpeg">
+<img width="100%" height="100%" src="p1.jpeg"></a></td><td style="border:0; color:#2F4F4F;" colspan="2" 
+align="left" valign="middle">1. Zaitseva</td></tr>
+<tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
+<tr><td style="background-color:#DC143C;" align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p></td>
 <td style="background-color:#6A5ACD;" align="center" valign="middle" width="25%"><p style="color:white;">PAU chok</p></td>
 <td style="background-color:#9ACD32;" align="center" valign="middle" width="25%"><p style="color:white;">PAU coco</p></td>
 <td style="background-color:gold;" align="center" valign="middle" width="25%"><p style="color:white;">PAU gold</p></td></tr>
-<tr><td align="center" valign="middle">1.250</td> <td align="center" valign="middle">964</td>
-<td align="center" valign="middle">15</td>
-<td align="center" valign="middle">3</td></tr>
+<tr><td align="center" valign="middle">1.250</td><td align="center" valign="middle">964</td>
+<td align="center" valign="middle">15</td><td align="center" valign="middle">3</td></tr>
 </table>
 
-<table border="1" colspan="4" width="100%" align="center" valign="middle" cellpadding="2"
-cellspacing="2">
-<tr><td style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"> <img width="100%" height="100%" 
-src="pauk3-1.jpg"></td><td style="border:0; color:#2F4F4F;" colspan="2" align="left" valign="middle"> 
-2. Vadim12345</td></tr><tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
-<tr><td style="background-color:#DC143C;"
-align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p></td>
+<table border="1" colspan="4" width="100%" align="center" valign="middle" cellpadding="2" cellspacing="2">
+<tr><td style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"><img width="100%" height="100%" 
+src="pauk3-1.jpg"></td><td style="border:0; color:#2F4F4F;" colspan="2" align="left" valign="middle">2. Vadim12345</td></tr>
+<tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
+<tr><td style="background-color:#DC143C;" align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p></td>
 <td style="background-color:#6A5ACD;" align="center" valign="middle" width="25%"><p style="color:white;">PAU chok</p></td>
 <td style="background-color:#9ACD32;" align="center" valign="middle" width="25%"><p style="color:white;">PAU coco</p></td>
 <td style="background-color:gold;" align="center" valign="middle" width="25%"><p style="color:white;">PAU gold</p></td></tr>
-<tr><td align="center" valign="middle">9</td> <td align="center" valign="middle">7</td>
+<tr><td align="center" valign="middle">9</td><td align="center" valign="middle">7</td>
 <td align="center" valign="middle">4</td>
 <td align="center" valign="middle">1</td></tr>
 </table>
 
-<table border="1" colspan="4" width="100%" align="center" valign="middle" cellpadding="2"
-cellspacing="2">
+<table border="1" colspan="4" width="100%" align="center" valign="middle" cellpadding="2"cellspacing="2">
 <tr><td style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"> <img width="100%" height="100%" 
 src="pauk3-1.jpg"></td><td style="border:0; color:#2F4F4F;"  colspan="2" align="left" valign="middle"> 
 3. </td></tr><tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
