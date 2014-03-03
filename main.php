353157ff64987859f082ea859d60c8e0557767c4
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-03 16:41:12 +0300

    Update main.php

--- main.php
+++ main.php
@@ -11,7 +11,8 @@ echo "Password: ".$_POST["password"];?></td</tr></table>
 <img width="100%" height="100%" src="p1.jpeg"></td><td style="border:0; color:#2F4F4F;" colspan="2" 
 align="left" valign="middle">1. Zaitseva</td></tr>
 <tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
-<tr><td style="background-color:#DC143C;" align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p></td>
+<tr><td style="background-color:#DC143C;" align="center" valign="middle" width="25%">
+<p style="color:white;">PAU tina</p></td>
 <td style="background-color:#6A5ACD;" align="center" valign="middle" width="25%"><p style="color:white;">PAU chok</p></td>
 <td style="background-color:#9ACD32;" align="center" valign="middle" width="25%"><p style="color:white;">PAU coco</p></td>
 <td style="background-color:gold;" align="center" valign="middle" width="25%"><p style="color:white;">PAU gold</p></td></tr>
