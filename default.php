4ccd2b3f8068690611f7bdda6aacd41fb38ddf52
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-05 00:03:39 +0300

    Update default.php

--- default.php
+++ default.php
@@ -1,30 +1,30 @@
 <?php $title=" МОБИЛЬНАЯ ПАУТИНКА ";require_once("header.php");?>
-
 <body style="padding:0;	margin:0;">
-
 <table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><p>&nbsp</p></td>
 <td style="background-color:#E6E6FA;" align="center" valign="middle"><a style="color:#0000CD;" 
-href="http://pautinka.mobi">pautinka.mobi</a>
-</td><td style="background-color:#E6E6FA;" align="center" valign="middle"><p>&nbsp</p></td>
+href="http://pautinka.mobi">pautinka.mobi</a></td>
+<td style="background-color:#E6E6FA;" align="center" valign="middle"><p>&nbsp</p></td>
 <td align="center" valign="middle"><a style="color:#DC143C;" href="http://pautinka.mobi/main.php">rus</a> | <a 
 style="color:#0000CD;" href="http://pautinka.mobi/torg.php">eng</a></td></tr>
+
 <tr><td colspan="4" align="center" valign="middle">
 <table border="0" width="100%" cellpadding="3" cellspacing="2">
 <tr><td style="border:0;" rowspan="6" width="7%" align="center" valign="middle"><p>&nbsp</p></td>
 <td style="border:0;" width="28%" align="center" valign="middle"><p>&nbsp</p></td>
 <td style="border:0;" rowspan="6" width="5%" align="center" valign="middle"><p>&nbsp</p></td>
 <td style="border:0;" rowspan="6" width="60%" align="center" valign="middle"><img alt="pautinka" width="100%"
-src="pauk.png"></td>
-</tr><tr><td style="border:0; background-color:#DC143C;" align="center" valign="middle">
-<p style="color:white">PAU tina</p></td></tr><tr><td style="border:0; background-color:#6A5ACD;" align="center"
-valign="middle"><p style="color:white">PAU chok</p></td></tr><tr><td style="border:0; background-color:#9ACD32;"
+src="pauk.png"></td></tr> 
+<tr><td style="border:0; background-color:#DC143C;" align="center" valign="middle">
+<p style="color:white">PAU tina</p></td></tr>
+<tr><td style="border:0; background-color:#6A5ACD;" align="center"
+valign="middle"><p style="color:white">PAU chok</p></td></tr>
+<tr><td style="border:0; background-color:#9ACD32;"
 align="center" valign="middle"><p style="color:white">PAU coco</p></td></tr>
 <tr><td style="border:0; background-color:gold;" align="center" valign="middle"><p style="color:white">PAU gold</p>
 </td></tr><tr><td align="center" valign="middle"><p>&nbsp</p></td></tr>
-</table>
+</table></td></tr>
 
-<table border="0" width="100%" cellpadding="0" cellspacing="0">
 <tr><td colspan="4" style="border:0; background-color:#FFFACD;" width="100%" align="center" valign="middle">
 <form action="http://pautinka.mobi/main.php" method="post">
 <table border="0" cellpadding="0" cellspacing="10">
@@ -34,6 +34,7 @@ align="center" valign="middle"><p style="color:white">PAU coco</p></td></tr>
 <td><input type="password" size=16 name="password"></td></tr>
 <tr><td align="center" valign="middle">&nbsp</td><td><input type="submit" name="submit" value="ВХОД"></td></tr>
 </table></form></td></tr>
+
 <tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr>
 <tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr>
 <?php require_once("footer.php");?>
