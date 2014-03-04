1c694795fb5e268e5fd9d238c5bd259956bedef1
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-04 21:44:19 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
-<!DOCTOR HTML PUBLIC "-//w3c//System HTML 4.01 Transitional//End" "http://www.w3c.org/Tr/HTML/loose.dtd">
+<!DOCTYPE HTML PUBLIC "-//w3c//System HTML 4.01 Transitional//End" "http://www.w3c.org/Tr/HTML/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>
 <?php echo $title;?>
 </title>
