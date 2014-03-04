abe110d4856db9b78d5dcc41f5d539b8790e38fd
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-04 22:31:53 +0300

    Update default.php

--- default.php
+++ default.php
@@ -2,7 +2,7 @@
 
 <body style="padding:0;	margin:0;">
 
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><p>&nbsp</p></td>
 <td style="background-color:#E6E6FA;" align="center" valign="middle"><a style="color:#0000CD;" 
 href="http://pautinka.mobi">pautinka.mobi</a>
@@ -10,11 +10,12 @@ href="http://pautinka.mobi">pautinka.mobi</a>
 <td align="center" valign="middle"><a style="color:#DC143C;" href="http://pautinka.mobi/main.php">rus</a> | <a 
 style="color:#0000CD;" href="http://pautinka.mobi/torg.php">eng</a></td></tr>
 
-<table border="0" width="100%" align="center" valign="middle" cellpadding="3" cellspacing="2">
+<table border="0" width="100%" cellpadding="3" cellspacing="2">
 <tr><td style="border:0;" rowspan="6" width="7%" align="center" valign="middle"><p>&nbsp</p></td>
 <td style="border:0;" width="28%" align="center" valign="middle"><p>&nbsp</p></td>
 <td style="border:0;" rowspan="6" width="5%" align="center" valign="middle"><p>&nbsp</p></td>
-<td style="border:0;" rowspan="6" width="60%" align="center" valign="middle"><img width="100%" src="pauk.png"/></td>
+<td style="border:0;" rowspan="6" width="60%" align="center" valign="middle"><img alt="pautinka" width="100%"
+src="pauk.png"></td>
 </tr><tr><td style="border:0; background-color:#DC143C;" align="center" valign="middle">
 <p style="color:white">PAU tina</p></td></tr><tr><td style="border:0; background-color:#6A5ACD;" align="center"
 valign="middle"><p style="color:white">PAU chok</p></td></tr><tr><td style="border:0; background-color:#9ACD32;"
@@ -23,7 +24,7 @@ align="center" valign="middle"><p style="color:white">PAU coco</p></td></tr>
 </td></tr><tr><td align="center" valign="middle"><p>&nbsp</p></td></tr>
 </table>
 
-<table border="0" width="100%" align="center" valign="middle" cellpadding="0" cellspacing="0">
+<table border="0" width="100%" cellpadding="0" cellspacing="0">
 <tr><td colspan="4" style="border:0; background-color:#FFFACD;" width="100%" align="center" valign="middle">
 <table border="0" cellpadding="0" cellspacing="10">
 <form action="http://pautinka.mobi/main.php" method="post"></td><td align="right" valign="middle">E-mail:</td>
