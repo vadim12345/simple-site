0440e347f6c7ca7a1ce24fa882962a9ade9b5c4f
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-05 16:48:46 +0300

    Update game.php

--- game.php
+++ game.php
@@ -3,14 +3,14 @@
 
 <table><tr><td colspan="2" align="center" valign="middle">&nbsp</td></tr></table>
 
-<tr><td colspan="2" align="center" valign="middle"><b>игра</b><br/>вид осмысленной
+<tr><td colspan="2" align="center" valign="middle"><h3>игра</h3><p>Вид осмысленной
 непродуктивной деятельности,
 где мотив лежит не в результате
 её, а в самом процессе. Также
 термин «Игра» используют для
 обозначения набора предметов
 или программ , предназначенных
-для подобной деятельности.</td></tr>
+для подобной деятельности.</p></td></tr>
 <tr><td colspan="2" align="center" valign="middle">&nbsp</td></tr>
 
 <?php require_once("footer.php");?>
