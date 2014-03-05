10a3f7e9593370e52461279aa5df525298429506
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-05 17:01:12 +0300

    Update dnevnik.php

--- dnevnik.php
+++ dnevnik.php
@@ -1,14 +1,10 @@
 <?php $title=" ДНЕВНИК ";require_once("header.php");?>
 <?php $img="x2.jpg";require_once("body.php");?>
 
-<table><tr><td colspan="2" align="center" valign="middle">&nbsp</td></tr></table>
-
-<tr><td colspan="2" align="center" valign="middle"><b>дневник</b><br/>
-совокупность
-фрагментарных записей, которые
-делаются для себя, ведутся
-регулярно и чаще всего
-сопровождаются указанием даты.</td></tr>
+<tr><td colspan="2" align="center" valign="middle">&nbsp</td></tr>
+<tr><td colspan="2" align="center" valign="middle">
+<h3>Дневник</h3><p>Совокупность фрагментарных записей, которые делаются для себя, ведутся
+регулярно и чаще всего сопровождаются указанием даты.</p></td></tr>
 <tr><td colspan="2" align="center" valign="middle">&nbsp</td></tr>
 
 <?php require_once("footer.php");?>
