6352e1ec6a6dc3d02de243ab1086bbddd42fefb2
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-05 16:32:44 +0300

    Update inform.php

--- inform.php
+++ inform.php
@@ -4,17 +4,17 @@
 <tr><td colspan="4" align="center" valign="middle"><p>Чем больше друзей приглосите в паутинку, тем
 больше голосов получаете и тем больше PAU у вас будет.</p></td></tr>
 <tr><td colspan="4" align="center" valign="middle">
-<h3><br/>Как это работает?</h3>
-После регистрации Вы получите
-специальную реферальную ссылку.<br/>
-<a href="http://pautinka.mobi">http://pautinka.mobi</a><br/>
-Разошлите её другим людям и как
+<h3>Как это работает?</h3>
+<p>После регистрации Вы получите
+специальную реферальную ссылку.</p>
+<a href="http://pautinka.mobi">http://pautinka.mobi</a>
+<p>Разошлите её другим людям и как
 только они зарегистрируются на
-pautinka.mobi
+pautinka.mobi</p>
 Вам будут зачислен 1 ГОЛОС.
 Чем больше людей Вы пригласите, 
-тем больше ГОЛОСОВ получите!<br/>
-3 ГОЛОСА = 1 ПАУТИНКA</td></tr>
+тем больше ГОЛОСОВ получите!
+<p>3 ГОЛОСА = 1 ПАУТИНКA</p></td></tr>
 <tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr>
 <?php require_once("footer.php");?>
 
