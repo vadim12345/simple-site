16193f8273b6ac0d8b131cd91d0038421f22587c
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-07 18:27:06 +0300

    Update main.php

--- main.php
+++ main.php
@@ -7,8 +7,8 @@ echo "Password: ".$_POST["password"];?></td</tr></table>
 <table><tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr></table>
 
 <table border="1" width="100%" cellpadding="2" cellspacing="2">
-<tr><td style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"><a style="border:0;" href="p1.jpeg">
-<img alt="pautinka" width="100%" height="100%" src="p1.jpeg"></a></td><td style="border:0; color:#2F4F4F;" colspan="2" 
+<tr><td style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"><a style="border:0;" href="pe2.jpg">
+<img alt="pautinka" width="100%" height="100%" src="pe2.jpg"></a></td><td style="border:0; color:#2F4F4F;" colspan="2" 
 align="left" valign="middle">1. Zaitseva</td></tr>
 <tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
 <tr><td style="background-color:#DC143C;" align="center" valign="middle" width="25%">
@@ -21,8 +21,9 @@ align="left" valign="middle">1. Zaitseva</td></tr>
 </table>
 
 <table border="1" width="100%" cellpadding="2" cellspacing="2">
-<tr><td style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"><img alt="pautinka" width="100%" height="100%" 
-src="pauk3-1.jpg"></td><td style="border:0; color:#2F4F4F;" colspan="2" align="left" valign="middle">2. Vadim12345</td></tr>
+<tr><td style="border:0;" rowspan="2" colspan="2" align="center" valign="middle">
+<a style="border:0;" href="pe1.jpg"><img alt="pautinka" width="100%" height="100%" 
+src="pe1.jpg"></td><td style="border:0; color:#2F4F4F;" colspan="2" align="left" valign="middle">2. Vadim12345</td></tr>
 <tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
 <tr><td style="background-color:#DC143C;" align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p></td>
 <td style="background-color:#6A5ACD;" align="center" valign="middle" width="25%"><p style="color:white;">PAU chok</p></td>
