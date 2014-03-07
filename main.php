c5d293e5f6100ebe9828252bfcb16bd0d1ef2eb6
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-07 19:02:13 +0300

    Update main.php

--- main.php
+++ main.php
@@ -7,7 +7,7 @@ echo "Password: ".$_POST["password"];?></td</tr></table>
 <table><tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr></table>
 
 <table border="1" width="100%" cellpadding="2" cellspacing="2">
-<tr><td height="70" style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"><a style="border:0;" href="pe2.jpg">
+<tr><td height="80" style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"><a style="border:0;" href="pe2.jpg">
 <img alt="pautinka" width="100%" height="100%" src="pe2.jpg"></a></td><td style="border:0; color:#2F4F4F;" colspan="2" 
 align="left" valign="middle">1. Zaitseva</td></tr>
 <tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
@@ -21,7 +21,7 @@ align="left" valign="middle">1. Zaitseva</td></tr>
 </table>
 
 <table border="1" width="100%" cellpadding="2" cellspacing="2">
-<tr><td height="75" style="border:0;" rowspan="2" colspan="2" align="center" valign="middle">
+<tr><td height="85" style="border:0;" rowspan="2" colspan="2" align="center" valign="middle">
 <a style="border:0;" href="pe1.jpg"><img alt="pautinka" width="100%" height="100%" 
 src="pe1.jpg"></td><td style="border:0; color:#2F4F4F;" colspan="2" align="left" valign="middle">2. Vadim12345</td></tr>
 <tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
