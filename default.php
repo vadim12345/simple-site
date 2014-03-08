5e166497f89f945e819a949dcebf1d5a52a89799
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-08 15:31:02 +0300

    Update default.php

--- default.php
+++ default.php
@@ -35,7 +35,7 @@ align="center" valign="middle"><p style="color:white">PAU coco</p></td></tr>
 <tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr>
 <tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr>
 <tr><td colspan="4" align="center" valign="middle"><a style="color:#9370D8;" 
-href="http://pautinka.mobi">Забыли&nbspпароль<b>?</b></a><br>&nbsp<br><a style="color:#9370D8;"
+href="http://pautinka.mobi">Забыли&nbspпароль<b style="color:red;">?</b></a><br>&nbsp<br><a style="color:#9370D8;"
 href="http://worldlibrary.bugs3.com">Связаться&nbspс&nbspавтором<b style="color:red;">?</b></a></td></tr>
 <tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr>
 <tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr>
