682916fbc6bacfed6f354a5ae425697564ba9aec
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-09 21:19:03 +0300

    Update password.php

--- password.php
+++ password.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head><title>АВТОР ПАУТИНКИ</title>
+<head><title>ПАРОЛЬ</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
 <body style="padding:0;	margin:0;">
@@ -10,10 +10,10 @@
 <tr><td  align="center" valign="middle">&nbsp</td></tr>
 
 <tr><td align="center" valign="middle">
-<form action="http://pautinka.mobi/avtor.php" method="POST"><fieldset><legend>&nbsp&nbspЗадать&nbspвопрос</legend> 
-<p>Имя</p><input size="24" maxlength="20" type ="text" name="name">
-<p>E-mail</p><input size="24" maxlenght="20" type="text" name ="email">
-<p><textarea name="text" cols="20" rows="6">Введите текст...</textarea></p>
+<form action="http://pautinka.mobi/password.php" method="POST"><fieldset><legend>&nbsp&nbspВосстановить&nbspпароль</legend> 
+<p> Введите свой адрес эл. почты.</p><input size="24" maxlength="20" type ="text" name="E-mail...">
+
+
 <p><input type="submit" name="submit" value="Отправить"></p>
 </fieldset></form></td></tr>
 
