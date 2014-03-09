a76d4b4e25e70d2c9beb9d1716a847fa573b996f
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-09 20:19:33 +0300

    Update password.php

--- password.php
+++ password.php
@@ -1,1 +1,26 @@
-Do
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head><title>АВТОР ПАУТИНКИ</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body style="padding:0;	margin:0;">
+<table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr><td style="background-color:#E6E6FA;" align="center" valign="middle">
+<a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
+<tr><td  align="center" valign="middle">&nbsp</td></tr>
+
+<tr><td align="center" valign="middle">
+<form action="http://pautinka.mobi/avtor.php" method="POST"><fieldset><legend>&nbsp&nbspЗадать&nbspвопрос</legend> 
+<p>Имя</p><input size="24" maxlength="20" type ="text" name="name">
+<p>E-mail</p><input size="24" maxlenght="20" type="text" name ="email">
+<p><textarea name="text" cols="20" rows="6">Введите текст...</textarea></p>
+<p><input type="submit" name="submit" value="Отправить"></p>
+</fieldset></form></td></tr>
+
+<tr><td align="center" valign="middle">&nbsp</td></tr>
+<tr><td style="background-color:#E6E6FA;" align="center" valign="middle"> 
+&copy; 2014 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
+</table>
+</body>
+</html>
+
