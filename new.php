1b410533515da7c6f86844ccebafddbf74759c8d
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-10 00:14:07 +0300

    Update new.php

--- new.php
+++ new.php
@@ -1,1 +1,12 @@
-DO
+<div id="cell_vxod">
+
+<div class="cell_pola_vxod"><div class="vxod_name">E-mail:</div><input class="login_pole"
+type="text" name="user" value=" " size="20" maxlength="50"></div>
+<div class="cell_pola_vxod"><div class="vxod_name">Password:</div><input class="login_pole"
+type="password" name="password" size="20" maxlength="16"></div>
+<div id="vxod_reg"><a href="$REMINDER_LINK$">Забыли пароль</a> | <a href="$REGISTER_LINK$">Регистрация</a>
+<span class="right"><input class="south" title="Сохранить пароль при входе"id="rem$PAGE_ID$" type="checkbox" name="rem"
+value="1" checked="checked"><label for="rem$PAGE_ID$"></label></span></div>
+<div id="cell_knopkav"><input class="knopka_vxoda" name="sbm" type="submit" value="Вход"></div>
+
+</div>
