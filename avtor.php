ee116bc7cc90c929f8354123bb347fb5abe818d8
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-09 19:21:23 +0300

    Update avtor.php

--- avtor.php
+++ avtor.php
@@ -5,7 +5,8 @@
 </head>
 <body style="padding:0;	margin:0;">
 <table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
-
+<tr><td style="background-color:#E6E6FA;" align="center" valign="middle">
+&copy; 2014 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
 <tr><td  align="center" valign="middle">&nbsp</td></tr>
 
 <tr><td align="center" valign="middle">
@@ -16,7 +17,9 @@
 <p><input type="submit" name="submit" value="Отправить"></p>
 </fieldset></form></td></tr>
 
-
+<tr><td align="center" valign="middle">&nbsp</td></tr>
+<tr><td style="background-color:#E6E6FA;" align="center" valign="middle"> 
+&copy; 2014 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
 </table>
 </body>
 </html>
