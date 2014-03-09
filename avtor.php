b5f63a30bb80216d9b59d33533544ef81fb45c16
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-09 17:29:34 +0300

    Update avtor.php

--- avtor.php
+++ avtor.php
@@ -12,8 +12,8 @@
 <tr><td align="center" valign="middle">
 <form action="http://pautinka.mobi/avtor.php" method="POST"><fieldset><legend>&nbspЗадать&nbspвопрос</legend> 
 
-<p>Имя</p><input size="25" maxlength="20" type ="text" name="name">
-<p>E-mail</p><input size="25" maxlenght="20" type="text" name ="email">
+<p>Имя</p><input size="24" maxlength="20" type ="text" name="name">
+<p>E-mail</p><input size="24" maxlenght="20" type="text" name ="email">
 <p><textarea name="text" cols="20" rows="6">Введите текст...</textarea></p>
 <p><input type="submit" name="submit" value="Отправить"></p>
 </fieldset></form></td></tr>
