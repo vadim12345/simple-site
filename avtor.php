60f46e09ed2841d73c555585a42e4fdb94b191df
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-09 19:16:13 +0300

    Update avtor.php

--- avtor.php
+++ avtor.php
@@ -1,22 +1,22 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head><title>АВТОР</title>
-<meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8">
+<head><title>АВТОР pautinka.mobi</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
 <body style="padding:0;	margin:0;">
-<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
+
 <tr><td  align="center" valign="middle">&nbsp</td></tr>
-<tr><td align="center" valign="middle">
-<a style="color:#0000CD;" href ="  ">
-<b>pugachev.vadim.v@gmail.com</b></td></tr>
+
 <tr><td align="center" valign="middle">
 <form action="http://pautinka.mobi/avtor.php" method="POST"><fieldset><legend>&nbspЗадать&nbspвопрос</legend> 
-
 <p>Имя</p><input size="24" maxlength="20" type ="text" name="name">
 <p>E-mail</p><input size="24" maxlenght="20" type="text" name ="email">
 <p><textarea name="text" cols="20" rows="6">Введите текст...</textarea></p>
 <p><input type="submit" name="submit" value="Отправить"></p>
 </fieldset></form></td></tr>
+
+
 </table>
 </body>
 </html>
