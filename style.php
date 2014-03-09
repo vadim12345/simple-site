c34a4794b9f37ca9c4efc639e1824956b062df72
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-09 13:58:53 +0300

    Update style.php

--- style.php
+++ style.php
@@ -1,1 +1,15 @@
-1
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head><title>АВТОР</title>
+<meta http-equiv = "Content-Type" content= "text/html; charset=UTF-8">
+</head>
+<body style="padding:0;	margin:0;">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr><td  align="center" valign="middle">&nbsp</td></tr>
+<tr><td align="center" valign="middle">
+<a style="color:#0000CD;" href ="  ">
+<b>pugachev.vadim.v@gmail.com</b></a>
+</td></tr>
+</table>
+</body>
+</html>
