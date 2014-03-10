a8a97015a695e1192d6ccc66303431c94f73f0cf
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-10 13:02:50 +0300

    Update new.php

--- new.php
+++ new.php
@@ -11,9 +11,9 @@
 <tr><td align="center" valign="middle">
 <div id="cell_vxod">
 <div class="cell_pola_vxod"><div class="vxod_name">E-mail:</div><input class="login_pole"
-type="text" name="user" value="привет" size="20" maxlength="50"></div>
+type="text" name="user" value=" " size="24" maxlength="20"></div>
 <div class="cell_pola_vxod"><div class="vxod_name">Password:</div><input class="login_pole"
-type="password" name="password" size="20" maxlength="16"></div>
+type="password" name="password" size="24" maxlength="20"></div>
 <div id="vxod_reg"><a href="$REMINDER_LINK$">Забыли пароль</a> | <a href="$REGISTER_LINK$">Регистрация</a>
 <span class="right"><input class="south" title="Сохранить пароль при входе"id="rem$PAGE_ID$" type="checkbox" name="rem"
 value="1" checked="checked"><label for="rem$PAGE_ID$"></label></span></div>
