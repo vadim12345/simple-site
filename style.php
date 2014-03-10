66911a3c481d47185c3b00ec3fa3174f309d69ab
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-10 13:34:05 +0300

    Update style.php

--- style.php
+++ style.php
@@ -1,1 +1,24 @@
-DO
+/* Форма входа
+------------------------------------------*/
+#cell_vxod {float:left; idth:270px;}
+#cell_vxod a:link,
+#cell_vxod a:visited {text-decoration:none; color:#5a79b8;}
+#cell_vxod a:hover {text-decoration:none; color:#5cb85a;}
+
+#vxod_reg {float:left; font:10px Verdana,Arial,Helvetica,sans-serif; color:#BABABA; font-weight:bold; width:212px;
+padding:5px 5px 0px 0px;}
+
+#cell_knopkav {float:left; width:53px;}.cell_pola_vxod {float:left; margin-bottom:10px;}
+.vxod_name {width:55px; text-shadow:1px 1px 1px #fff; background:#bdd5e4; border-right: none;
+border-left: 1px solid #97B7CA;
+padding: 5px 0px 5px 5px;}.login_pole {width:193px; border-left:none; border-right:1px solid #97B7CA;
+padding:5px 5px 5px 10px; vertical-align:middle;}.login_pole,.vxod_name{float:left; font:11px Verdana,Arial,Helvetica,
+sans-serif; color:#5789af; border-top:1px solid #97B7CA; border-bottom:1px solid #fff; margin:0px;}.login_pole {background: #eff3f7 url(http://www.center-dm.ru/ucoz/
+forma_vxoda/2/
+fon_ugol_pola_Blue.gif) no-repeat
+left;}
+.login_pole:focus {background:#f4f9ff url(http://www.center-dm.ru/ucoz/forma_vxoda/2/fon_ugol_pola_Blue.gif) no-repeat
+left;}.knopka_vxoda{color:#5789af; text-shadow:1px1px 1px #fff; padding: 3px 0px; width:53px;
+background: #eff3f7; border:1px solid #97B7CA; margin:0px;}
+.knopka_vxoda:hover{background: #bdd5e4;}
+/* -------------------------------------- */
