ba45c85c457f31c4c92788e5fa3d3b5f2245e9d5
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-10 23:07:12 +0300

    Update default.php

--- default.php
+++ default.php
@@ -1,17 +1,17 @@
 <?php $title=" МОБИЛЬНАЯ ПАУТИНКА ";require_once("header.php");?>
 <body style="padding:0;	margin:0;">
 <table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><p>&nbsp</p></td>
+<tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><p>&nbsp;</p></td>
 <td style="background-color:#E6E6FA;" align="center" valign="middle"><a style="color:#0000CD;" 
 href="http://pautinka.mobi">pautinka.mobi</a></td>
-<td style="background-color:#E6E6FA;" align="center" valign="middle"><p>&nbsp</p></td>
+<td style="background-color:#E6E6FA;" align="center" valign="middle"><p>&nbsp;</p></td>
 <td align="center" valign="middle"><a style="color:#DC143C;" href="http://pautinka.mobi/avtor.php">rus</a> | <a 
 style="color:#0000CD;" href="http://pautinka.mobi/new.php">eng</a></td></tr>
 <tr><td colspan="4" align="center" valign="middle">
 <table border="0" width="100%" cellpadding="3" cellspacing="2">
-<tr><td style="border:0;" rowspan="6" width="7%" align="center" valign="middle"><p>&nbsp</p></td>
-<td style="border:0;" width="28%" align="center" valign="middle"><p>&nbsp</p></td>
-<td style="border:0;" rowspan="6" width="5%" align="center" valign="middle"><p>&nbsp</p></td>
+<tr><td style="border:0;" rowspan="6" width="7%" align="center" valign="middle"><p>&nbsp;</p></td>
+<td style="border:0;" width="28%" align="center" valign="middle"><p>&nbsp;</p></td>
+<td style="border:0;" rowspan="6" width="5%" align="center" valign="middle"><p>&nbsp;</p></td>
 <td style="border:0;" rowspan="6" width="60%" align="center" valign="middle"><img alt="pautinka" width="100%"
 src="pauk.png"></td></tr> 
 <tr><td style="border:0; background-color:#DC143C;" align="center" valign="middle">
@@ -21,21 +21,21 @@ valign="middle"><p style="color:white">PAU chok</p></td></tr>
 <tr><td style="border:0; background-color:#9ACD32;"
 align="center" valign="middle"><p style="color:white">PAU coco</p></td></tr>
 <tr><td style="border:0; background-color:gold;" align="center" valign="middle"><p style="color:white">PAU gold</p>
-</td></tr><tr><td align="center" valign="middle"><p>&nbsp</p></td></tr>
+</td></tr><tr><td align="center" valign="middle"><p>&nbsp;</p></td></tr>
 </table></td></tr>
-<tr><td colspan="4" style="border:0; background-color:#FFFACD;" width="100%" align="center" valign="middle">&nbsp 
+<tr><td colspan="4" style="border:0; background-color:#FFFACD;" width="100%" align="center" valign="middle">
 <form action="http://pautinka.mobi/main.php" method="post">
 <table border="0" cellpadding="0" cellspacing="7"><tr><td align="right" valign="middle">E-mail:</td>
 <td><input type="text" size=16 name="e-mail"></td></tr><tr><td align="right" valign="middle">Password:</td>
 <td><input type="password" size=16 name="password"></td></tr>
-<tr><td align="center" valign="middle">&nbsp</td><td><input type="submit" name="submit" value="ВХОД">&nbspВ системе 
+<tr><td align="center" valign="middle">&nbsp;</td><td><input type="submit" name="submit" value="ВХОД">&nbsp;&nbsp;&nbsp;&nbsp;
 <span class="right"><input class="south" title="Сохранить пароль при входе"id="rem$PAGE_ID$" type="checkbox" name="rem"
-value="1" checked="checked"><label for="rem$PAGE_ID$"></label></span><br>&nbsp</td></tr></table>
-<tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr>
-<tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr>
+value="1" checked="checked"><label for="rem$PAGE_ID$"></label></span><br>&nbsp;</td></tr></table>
+<tr><td colspan="4" align="center" valign="middle">&nbsp;</td></tr>
+<tr><td colspan="4" align="center" valign="middle">&nbsp;</td></tr>
 <tr><td colspan="4" align="center" valign="middle">
-<a style="color:#9370D8;" href="http://pautinka.mobi/password.php">Забыли&nbspпароль&nbsp?</a><br>&nbsp<br>
-<a style="color:#9370D8;" href="http://pautinka.mobi/avtor.php">&nbspСвязаться&nbspс&nbspавтором&nbsp!</a></td></tr>
-<tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr>
-<tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr>
+<a style="color:#9370D8;" href="http://pautinka.mobi/password.php">Забыли&nbsp;пароль&nbsp;?</a><br>&nbsp;<br>
+<a style="color:#9370D8;" href="http://pautinka.mobi/avtor.php">&nbsp;Связаться&nbsp;с&nbsp;автором&nbsp;!</a></td></tr>
+<tr><td colspan="4" align="center" valign="middle">&nbsp;</td></tr>
+<tr><td colspan="4" align="center" valign="middle">&nbsp;</td></tr>
 <?php require_once("footer.php");?>
