b8e93a937e5f10ca050c39e383faff30f8fa2221
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-10 12:38:33 +0300

    Update new.php

--- new.php
+++ new.php
@@ -1,12 +1,30 @@
-<div id="cell_vxod">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head><title>ПАУТИНКА</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body style="padding:0;	margin:0;">
+<table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr><td style="background-color:#E6E6FA;" align="center" valign="middle">
+<a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
+<tr><td  align="center" valign="middle">&nbsp</td></tr>
 
+<div id="cell_vxod">
 <div class="cell_pola_vxod"><div class="vxod_name">E-mail:</div><input class="login_pole"
-type="text" name="user" value=" " size="20" maxlength="50"></div>
+type="text" name="user" value="привет" size="20" maxlength="50"></div>
 <div class="cell_pola_vxod"><div class="vxod_name">Password:</div><input class="login_pole"
 type="password" name="password" size="20" maxlength="16"></div>
 <div id="vxod_reg"><a href="$REMINDER_LINK$">Забыли пароль</a> | <a href="$REGISTER_LINK$">Регистрация</a>
 <span class="right"><input class="south" title="Сохранить пароль при входе"id="rem$PAGE_ID$" type="checkbox" name="rem"
 value="1" checked="checked"><label for="rem$PAGE_ID$"></label></span></div>
 <div id="cell_knopkav"><input class="knopka_vxoda" name="sbm" type="submit" value="Вход"></div>
-
 </div>
+<tr><td align="center" valign="middle">&nbsp</td></tr>
+<tr><td style="background-color:#E6E6FA;" align="center" valign="middle"> 
+&copy; 2014 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
+</table>
+</body>
+</html>
+
+
+
