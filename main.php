c7e7d2f2075efa7086b877ccde21b2d1e02d0663
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-11 18:57:11 +0300

    Update main.php

--- main.php
+++ main.php
@@ -8,7 +8,7 @@ echo "Password: ".$_POST["password"];?></td</tr></table>
 
 <table border="1" width="100%" height="140" cellpadding="2" cellspacing="2">
 <tr><td height="80" style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"><a style="border:0;" href="pe2.jpg">
-<img alt="pautinka" width="100%" height="100%" src="pe2.jpg"></a></td><td style="border:0; color:#2F4F4F;" colspan="2" 
+<img alt="pautinka" width="100%" height="100%" src="imeg/pe2.jpg"></a></td><td style="border:0; color:#2F4F4F;" colspan="2" 
 align="left" valign="middle">1. Zaitseva</td></tr>
 <tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
 <tr><td style="background-color:#DC143C;" align="center" valign="middle" width="25%">
@@ -23,7 +23,7 @@ align="left" valign="middle">1. Zaitseva</td></tr>
 <table border="1" width="100%" height="140" cellpadding="2" cellspacing="2">
 <tr><td height="80" style="border:0;" rowspan="2" colspan="2" align="center" valign="middle">
 <a style="border:0;" href="pe1.jpg"><img alt="pautinka" width="100%" height="100%" 
-src="pe1.jpg"></td><td style="border:0; color:#2F4F4F;" colspan="2" align="left" valign="middle">2. Vadim12345</td></tr>
+src="imeg/pe1.jpg"></td><td style="border:0; color:#2F4F4F;" colspan="2" align="left" valign="middle">2. Vadim12345</td></tr>
 <tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
 <tr><td style="background-color:#DC143C;" align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p></td>
 <td style="background-color:#6A5ACD;" align="center" valign="middle" width="25%"><p style="color:white;">PAU chok</p></td>
@@ -37,7 +37,7 @@ src="pe1.jpg"></td><td style="border:0; color:#2F4F4F;" colspan="2" align="left"
 <table border="1" width="100%" height="140" cellpadding="2"cellspacing="2">
 <tr><td height="80" style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"> 
 <a style="border:0;" href="pauk3-1.jpg"><img alt="pautinka" width="100%" height="100%" 
-src="pauk3-1.jpg"></td><td style="border:0; color:#2F4F4F;"  colspan="2" align="left" valign="middle"> 
+src="imeg/pauk3-1.jpg"></td><td style="border:0; color:#2F4F4F;"  colspan="2" align="left" valign="middle"> 
 3. </td></tr><tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
 <tr><td style="background-color:#DC143C;"
 align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p></td>
@@ -53,7 +53,7 @@ align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p><
 cellspacing="2">
 <tr><td height="80" style="border:0;" rowspan="2" colspan="2" align="center" valign="middle">
 <a style="border:0;" href="pauk3-1.jpg"><img alt="pautinka" width="100%" height="100%" 
-src="pauk3-1.jpg"></td><td style="border:0; color:#2F4F4F;"  colspan="2" align="left" valign="middle"> 
+src="imeg/pauk3-1.jpg"></td><td style="border:0; color:#2F4F4F;"  colspan="2" align="left" valign="middle"> 
 4. </td></tr><tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
 <tr><td style="background-color:#DC143C;"
 align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p></td>
@@ -68,7 +68,7 @@ align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p><
 <table border="1" width="100%" height="140" cellpadding="2" cellspacing="2">
 <tr><td height="80" style="border:0;" rowspan="2" colspan="2" align="center" valign="middle"> 
 <a style="border:0;" href="pauk3-1.jpg"><img alt="pautinka" width="100%" height="100%" 
-src="pauk3-1.jpg"></td><td style="border:0; color:#2F4F4F;" colspan="2" align="left" valign="middle"> 
+src="imeg/pauk3-1.jpg"></td><td style="border:0; color:#2F4F4F;" colspan="2" align="left" valign="middle"> 
 5. </td></tr><tr><td style="border:0;" colspan="2" align="center" valign="middle">&nbsp</td></tr>
 <tr><td style="background-color:#DC143C;"
 align="center" valign="middle" width="25%"><p style="color:white;">PAU tina</p></td>
