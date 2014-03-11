ebd7e3850839bb97a98100f706dc95b67e1788cc
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-11 14:24:55 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <!DOCTYP HTML PUBLIC "-//W3C//DTD HTML 4.01Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<link rel='stylesheet' type='text/css' href='style.css'/>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>
 <?php echo $title;?>
