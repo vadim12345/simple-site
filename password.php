09f492a49ff707dc4600f0a58450d0aceeea9771
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-12 15:19:01 +0300

    Update password.php

--- password.php
+++ password.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head><title>ПАРОЛЬ</title>
+<head><title>ВОССТАНОВИТЬ ПАРОЛЬ</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
 <body style="padding:0;	margin:0;">
@@ -10,7 +10,7 @@
 <tr><td  align="center" valign="middle">&nbsp</td></tr>
 
 <tr><td align="center" valign="middle">
-<form action="http://pautinka.mobi/password.php" method="POST"><fieldset><legend>&nbsp&nbspВосстановить пароль&nbsp;!</legend> 
+<form action="http://pautinka.mobi/password.php" method="POST"><fieldset><legend>&nbsp&nbspВосстановить&nbsp;пароль&nbsp;!</legend> 
 
 <p align="center" valign="middle"><a style="border:0;" href="images/an.png"><img alt="pautinka"  
 src="images/an.png"></a></p>
