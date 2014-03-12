104041965e14a38d1c2ace95b29cd9d338e036d7
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-12 15:11:57 +0300

    Update password.php

--- password.php
+++ password.php
@@ -12,8 +12,8 @@
 <tr><td align="center" valign="middle">
 <form action="http://pautinka.mobi/password.php" method="POST"><fieldset><legend>&nbsp&nbspВосстановить пароль&nbsp;!</legend> 
 
-<tr><th class="zagalovok"><a style="border:0;" href="images/an.png"><img alt="pautinka"  
-src="images/an.png"></a></th></tr>
+<p align="center" valign="middle"><a style="border:0;" href="images/an.png"><img alt="pautinka"  
+src="images/an.png"></a></p>
 <p> Введите свой адрес эл. почты.</p><input size="24" maxlength="20" type ="text" name="E-mail...">
 <p><input type="submit" name="submit" value="Отправить"></p>
 </fieldset></form></td></tr>
