f217af3c402e9ee89b3ef3f1f56abee470367889
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-12 15:39:28 +0300

    Update password.php

--- password.php
+++ password.php
@@ -7,10 +7,10 @@
 <table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle">
 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
-<tr><td  align="center" valign="middle">&nbsp</td></tr>
+<tr><td  align="center" valign="middle">&nbsp;</td></tr>
 
 <tr><td align="center" valign="middle">
-<form action="http://pautinka.mobi/password.php" method="POST"><fieldset><legend>&nbsp&nbspВосстановить пароль&nbsp;!</legend> 
+<form action="http://pautinka.mobi/password.php" method="POST"><fieldset><legend>&nbsp;&nbsp;Восстановить&nbsp;пароль&nbsp;!</legend> 
 
 <p align="center" valign="middle"><a style="border:0;" href="images/an.png"><img alt="pautinka"  
 src="images/an.png"></a></p>
@@ -18,11 +18,11 @@ src="images/an.png"></a></p>
 <p><input type="submit" name="submit" value="Отправить"></p>
 </fieldset></form></td></tr>
 
-<tr><td align="center" valign="middle">&nbsp</td></tr>
+<tr><td align="center" valign="middle">&nbsp;</td></tr>
 <tr><td align="center" valign="middle">
-<p>Не получилось восстановить пароль&nbsp?</p><p>Обратитесь&nbsp&nbspв <a style="color:#9370D8;"
-href="http://pautinka.mobi/avtor.php">службу&nbspподдержки.</a></p></td></tr>
-<tr><td align="center" valign="middle">&nbsp</td></tr>
+<p>Не получилось восстановить пароль&nbsp?</p><p>Обратитесь&nbsp;&nbsp;в <a style="color:#9370D8;"
+href="http://pautinka.mobi/avtor.php">службу&nbsp;поддержки.</a></p></td></tr>
+<tr><td align="center" valign="middle">&nbsp;</td></tr>
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle"> 
 &copy; 2014 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
 </table>
