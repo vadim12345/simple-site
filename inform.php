b6a3e9992e96273102c3179b285ace7fc498aed6
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-15 12:54:29 +0300

    Update inform.php

--- inform.php
+++ inform.php
@@ -3,8 +3,8 @@
 <tr><td colspan="4" align="center" valign="middle">&nbsp;</td></tr>
 <tr><td colspan="4" align="center" valign="middle">
 <p><fieldset><legend>&nbsp;&nbsp;PAU purse&nbsp;</legend>
-<tr><td align="center"><a style="color:#0000CD;" href="http://www.pautinka.mobi/new.php">
-создать&nbsp;бесплатный&nbsp;кошелёк</a></td></tr></fieldset></a></p>
+<a style="color:#0000CD;" href="http://www.pautinka.mobi/new.php">
+создать&nbsp;бесплатный&nbsp;кошелёк</a></fieldset></a></p>
 <p>pautinka символизирует порядок и равновесие сил, однако основное значение символа — связывать и удерживать.</p>
 <p>PAU можно заработать пригласив множество друзей ,а также можно купить на торговой площадке.</p> 
 <p>Существует 4 вида PAU:<br><ul><li>PAU tina - связывает и удерживает...</li>
