4622c28b1a67101393dd7f9769bf3df99825a37f
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-15 12:32:28 +0300

    Update inform.php

--- inform.php
+++ inform.php
@@ -2,14 +2,14 @@
 <?php $img="imeg/x2.jpg";require_once("body.php");?>
 <tr><td colspan="4" align="center" valign="middle">&nbsp;</td></tr>
 <tr><td colspan="4" align="center" valign="middle">
-<p><a style="color:#0000CD;" href="http://www.pautinka.mobi/new.php"><fieldset style="color:#0000CD;"><legend>&nbsp;&nbsp;PAU purse&nbsp;</legend>
-Создать бесплатный кошелёк</fieldset></a></p>
+<p><a href="http://www.pautinka.mobi/new.php"><fieldset><legend>&nbsp;&nbsp;PAU purse&nbsp;</legend>
+<b style="color:#0000CD;">создать бесплатный кошелёк</b></fieldset></a></p>
 <p>pautinka символизирует порядок и равновесие сил, однако основное значение символа — связывать и удерживать.</p>
 <p>PAU можно заработать пригласив множество друзей ,а также можно купить на торговой площадке.</p> 
 <p>Существует 4 вида PAU:<br><ul><li>PAU tina - связывает и удерживает...</li>
 <li>PAU chok - отвечает за безопасность...</li><li>PAU coco - кокон производит PAU...</li>
-<li>PAU gold - подкрепленно золотом...</li></ul><p>Стоимость:<br>1 PAU tina=3.3$<br>1 PAU chok=33$<br>1 PAU coco=330$<br>
-1 PAU gold=3.300$</p>ИГРА где зарабатываются PAU, обыграв своих друзей</p><P>ДНЕВНИК для ведения ежедневных записей...</P>
+<li>PAU gold - подкрепленно золотом...</li></ul><p>Стоимость:<br>1 PAU tina - 3.3$<br>1 PAU chok - 33$<br>1 PAU coco - 330$<br>
+1 PAU gold - 3.300$</p>ИГРА где зарабатываются PAU, обыграв своих друзей</p><P>ДНЕВНИК для ведения ежедневных записей...</P>
 <p>Чем больше друзей приглосите в паутинку, тем
 больше голосов получаете и тем больше PAU у вас будет.</p></td></tr>
 <tr><td colspan="4" align="center" valign="middle">
