6d5f24dcd37db2c2a6edb9e35d77496319114c42
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-15 17:05:18 +0300

    Update main.php

--- main.php
+++ main.php
@@ -7,8 +7,8 @@ echo "Password: ".$_POST["password"];?></td</tr></table>
 <table><tr><td colspan="4" align="center" valign="middle">&nbsp</td></tr></table>
 <table><tr><td colspan="4" align="center" valign="middle"><h4>ДОБРО&nbsp;ПОЖАЛОВАТЬ&nbsp;!</h4></td></tr>
 <tr><td colspan="4" align="center" valign="middle">&nbsp;</td></tr>
-<tr><td><p align="center"><div>Используйте&nbsp;PAU&nbsp;для&nbsp;того,&nbsp;чтобы<br/>расплачиваться&nbsp;и&nbsp;
-получать&nbsp;оплату<br/>просто&nbsp;и&nbsp;доступно.</div><p></td></tr>
+<tr><td><div align="center">Используйте&nbsp;PAU&nbsp;для&nbsp;того,&nbsp;чтобы<br/>расплачиваться&nbsp;и&nbsp;
+получать&nbsp;оплату<br/>просто&nbsp;и&nbsp;доступно.</div></td></tr>
 
 <tr><td colspan="4" align="center" valign="middle">
 <p><fieldset><legend>&nbsp;&nbsp;PAU purse&nbsp;</legend><table class="table2"><tr><td align="center">
