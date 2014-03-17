92770e1eb0d37eeb2fed2df014b8a393da5d4095
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-17 17:50:30 +0300

    Update default.php

--- default.php
+++ default.php
@@ -2,7 +2,7 @@
 <body style="padding:0;	margin:0;">
 <table style="height:100%;" width="100%" border="1" cellpadding="0" cellspacing="0">
 
-<tr><td style="background-color:#E6E6FA;" align="center" valign="middle"><a style="color:#0000CD;" href="http://pautinka.mobi">pautinka.mobi</a></td>
+<tr><td colspan="4" style="background-color:#E6E6FA;" align="center" valign="middle"><a style="color:#0000CD;" href="http://pautinka.mobi">pautinka.mobi</a></td>
 <td align="center" valign="middle"><a style="color:#DC143C;" href="http://pautinka.mobi/avtor.php">rus</a> | <a style="color:#0000CD;" href="http://pautinka.mobi/new.php">eng</a></td></tr>
 <tr><td align="center" valign="middle">
 <table border="1" width="100%" cellpadding="0" cellspacing="0">
@@ -41,5 +41,5 @@ value="1" checked="checked"><label for="rem$PAGE_ID$"></label>&nbsp;Запомн
 <a style="color:#4682B4;" href="http://pautinka.mobi/dogovor.php">&nbsp;пользовательское&nbsp;соглашение</a></td></tr></table></td></tr>
 <tr><td colspan="4" align="center" valign="middle">&nbsp;</td></tr>
 <tr><td colspan="4" align="center" valign="middle">&nbsp;</td></tr>
-</td></tr></table>
+</td></tr>
 <?php require_once("footer.php");?>
