99e5bcda4ed302f30732d1bf728680b61acabebf
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-17 23:34:18 +0300

    Update dogovor.php

--- dogovor.php
+++ dogovor.php
@@ -5,11 +5,11 @@
 </head>
 <body style="padding:0;	margin:0;">
 <table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
-<tr><td>
+
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle">
 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
 <tr><td align="center">
-<h5>Пользовательское соглашение.</h5>
+<div align=center" width="250"><h5>Пользовательское соглашение.</h5>
 Настоящее Соглашение (далее —
 Соглашение) регулирует отношения
 между pautinka.mobi -
@@ -636,11 +636,11 @@ pautinka.mobi вправе в
 уведомлений.<br/>
 <h5>6.  Подпись пользователя.</h5> 
 6.1. Регистрируясь на сайте pautinka.mobi пользователь автоматически подписывает пользовательское соглашение приняв все его условия.   
-<h5>Пользовательское соглашение.</h5>
+<h5>Пользовательское соглашение.</h5></div>
 </td></tr>
 <tr><td align="center" valign="middle">&nbsp;</td></tr>
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle"> 
-&copy; 2014 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
+&copy; 2014 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a>
 </td></tr></table>
 </body>
 </html>
