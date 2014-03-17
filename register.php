b87f743647b2bdd58e719e536ed9b53fab947918
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-17 22:42:37 +0300

    Update register.php

--- register.php
+++ register.php
@@ -4,15 +4,17 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
 <body style="padding:0;	margin:0;">
-<table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
+<table style="height:100%; width:100%;" border="0" cellpadding="0" cellspacing="0">
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle">
 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
-<tr><td  align="center" valign="middle">&nbsp;</td></tr>
-<table width="180" border="0" align="center"><td bgcolor="" align="center" valign="middle">
-<a style="color:#4682B4;" href="http://pautinka.mobi/dogovor.php">пользовательское&nbsp;соглашение</a></td></tr>
 
-<table><tr><td colspan="4" align="center" valign="middle"><?php echo "E-mail:<strong>".$_POST["e-mail"]."</strong><br/>";
-echo "Password: ".$_POST["password"];?></td</tr></table>
+<tr><td  align="center" valign="middle">&nbsp;</td></tr>
+
+<table width="180" border="0" align="center"><td bgcolor="" align="center" valign="middle">
+<a style="color:#4682B4;" href="http://pautinka.mobi/dogovor.php">пользовательское&nbsp;соглашение</a></td></tr></table>
+
+<tr><td align="center" valign="middle"><?php echo "E-mail:<strong>".$_POST["e-mail"]."</strong><br/>";
+echo "Password: ".$_POST["password"];?></td</tr>
 
 <tr><td align="center" valign="middle">&nbsp;</td></tr>
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle"> 
