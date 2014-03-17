5d8ad65287b2cef51522bdc8c9bafe22732a001c
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-17 21:20:25 +0300

    Create regisrt.php

---
+++ regisrt.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head><title>РЕГИСТРАЦИЯ</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body style="padding:0;	margin:0;">
+<table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr><td style="background-color:#E6E6FA;" align="center" valign="middle">
+<a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
+<tr><td  align="center" valign="middle">&nbsp;</td></tr>
+
+
+
+<tr><td align="center" valign="middle">&nbsp;</td></tr>
+<tr><td style="background-color:#E6E6FA;" align="center" valign="middle"> 
+&copy; 2014 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
+</table>
+</body>
+</html>
+
