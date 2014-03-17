9f04de7b221d459c802dd2584058980ed6fc860c
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-17 22:31:50 +0300

    Update default.php

--- default.php
+++ default.php
@@ -24,7 +24,7 @@ align="center" valign="middle"><p style="color:white">PAU coco</p></td></tr>
 </td></tr><tr><td align="center" valign="middle"><p>&nbsp;</p></td></tr>
 </table></td></tr>
 <tr><td colspan="4" style="border:0; background-color:#FFFACD;" width="100%" align="center" valign="middle">
-<form action="http://pautinka.mobi/registr.php" method="post">&nbsp;
+<form action="http://pautinka.mobi/register.php" method="post">&nbsp;
 <table border="0" cellpadding="0" cellspacing="7"><tr><td align="right" valign="middle">E-mail:</td>
 <td><input type="text" size=16 name="e-mail"></td></tr><tr><td align="right" valign="middle">Password:</td>
 <td><input type="password" size=16 name="password"></td></tr>
