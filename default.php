9e92d0c579fda749b5f91ef2527e5e2beae41b90
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-17 21:45:09 +0300

    Update default.php

--- default.php
+++ default.php
@@ -38,7 +38,7 @@ value="1" checked="checked"><label for="rem$PAGE_ID$"></label>&nbsp;Запомн
 <a style="color:#4682B4;" href="http://pautinka.mobi/password.php">Забыли&nbsp;пароль&nbsp;?</a><br/>&nbsp;<br/>
 <a style="color:#4682B4;" href="http://pautinka.mobi/avtor.php">Служба&nbsp;поддержки&nbsp;!</a><br/>&nbsp;<br/>
 <tr><td colspan="4" align="center" valign="middle">&nbsp;</td></tr>
-<tr><td colspan="4" align="center" valign="middle">&nbsp;</td></tr>
+
 <?php require_once("footer.php");?>
 
 
