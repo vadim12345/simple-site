1eb11b2c42ad5d74eff74c870b6360dd92cf87ca
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-17 22:49:01 +0300

    Update register.php

--- register.php
+++ register.php
@@ -7,15 +7,12 @@
 <table style="height:100%; width:100%;" border="0" cellpadding="0" cellspacing="0">
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle">
 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
-
 <tr><td  align="center" valign="middle">&nbsp;</td></tr>
-
 <table width="180" border="0" align="center"><td bgcolor="" align="center" valign="middle">
 <a style="color:#4682B4;" href="http://pautinka.mobi/dogovor.php">пользовательское&nbsp;соглашение</a></td></tr></table>
-
+<table>
 <tr><td align="center" valign="middle"><?php echo "E-mail:<strong>".$_POST["e-mail"]."</strong><br/>";
-echo "Password: ".$_POST["password"];?></td</tr>
-
+echo "Password: ".$_POST["password"];?></td</tr></table>
 <tr><td align="center" valign="middle">&nbsp;</td></tr>
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle"> 
 &copy; 2014 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
