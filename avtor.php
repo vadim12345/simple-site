2e13524036c6fda0cd1bd8a5cca0cdfee9a4ce9f
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-18 16:30:18 +0300

    Update avtor.php

--- avtor.php
+++ avtor.php
@@ -4,14 +4,14 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
 <body style="padding:0;	margin:0;">
-<table style="height:100%;" width="100%" border="0" cellpadding="0" cellspacing="0">
+<table style="height:100%;" width="100%" border="1" cellpadding="0" cellspacing="0">
 <tr><td style="background-color:#E6E6FA;" align="center" valign="middle">
 <a style="color:#0000CD;" href="http://www.pautinka.mobi">pautinka.mobi</a></td></tr>
 <tr><td  align="center" valign="middle">&nbsp;</td></tr>
  <tr><td align="center" valign="middle"><center>
  <form action="send.php" method="post"><fieldset><legend>&nbsp;&nbsp;Служба&nbsp;поддержки&nbsp;!</legend> 
  <table class="table2">
- <tbody><tr><th colspan="2" class="zagalovok"><a style="border:0;" href="images/an.png"><img alt="pautinka"  
+ <tbody><tr><th colspan="2" class="zagalovok"><a style="border:1;" href="images/an.png"><img alt="pautinka"  
 src="images/an.png"></a></th></tr><br>
  <tr><th colspan="2"></th></tr>
  <tr><td colspan="2" class="lable">*Ваше имя:</td></tr>
@@ -21,17 +21,9 @@ src="images/an.png"></a></th></tr><br>
  <tr><td colspan="2" class="lable">*Сообщение:</td></tr>
  <tr><td colspan="2" class="input"><textarea name="text_message" cols="20" rows="7" class="input"></textarea></td></tr>
  <tr><td align="center"  class="lable" valign="bootom">*Введите цифры: </td></tr>
- <tr><td align="center" valign="top"><?php
-$i=1;
-do
-{
-$num[$i] = mt_rand(0,9);
-echo "<img src='img/".$num[$i].".png' border='0' align='center' vspace='5px'>";
-$i++;
-}
-while ($i<5);
-$captcha = $num[1].$num[2].$num[3].$num[4];
-?>
+ <tr><td align="center" valign="top">
+<?php $i=1; do {$num[$i] = mt_rand(0,9); echo "<img src='img/".$num[$i].".png' border='0' align='center' vspace='5px'>";
+++;} while ($i<5); $captcha = $num[1].$num[2].$num[3].$num[4];?>
 <input name="captcha" type="hidden" value="<?php echo $captcha ;?>"><br/> 
 <input name="pr" style="margin-bottom:11px;" type="text" size="24" maxlength="4"></td></tr>
 <tr><th colspan="2"></th></tr>
