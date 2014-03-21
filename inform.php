f5a69f22487098608dd089dab6371c0bcdd54f8e
vadim12345 <vadim12345@users.noreply.github.com>
2014-03-21 22:34:41 +0300

    Update inform.php

--- inform.php
+++ inform.php
@@ -7,16 +7,16 @@
 <tr><td colspan="4" valign="middle">
 <ul><li>PAU tina - связывает и удерживает...</li>
 <li>PAU chok - отвечает за безопасность...</li><li>PAU coco - кокон производит PAU...</li>
-<li>PAU gold - подкрепленно золотом...</li></ul></td></tr><be/>
+<li>PAU gold - подкрепленно золотом...</li></ul></td></tr><br/>&nbsp;<br/>
 <tr><td colspan="4" align="center" valign="middle">
 <h5>Стоимость:</h5></td></tr>
 <tr><td colspan="4" valign="middle">
-<p><ol><li>PAU tina - 3.3 $</li><br><li>PAU chok - 33 $</li><br><li>PAU coco - 330 $</li><br>
-<li>PAU gold - 3.300 $</li></ol></td></tr><be/>
+<p><ol><li>PAU tina - 3.3 $</li><li>PAU chok - 33 $</li><li>PAU coco - 330 $</li>
+<li>PAU gold - 3.300 $</li></ol></td></tr><br/>&nbsp;<br/>
 <tr><td colspan="4" align="center" valign="middle">
 </p>ИГРА где зарабатываются PAU, обыграв своих друзей</p><P>ДНЕВНИК для ведения ежедневных записей...</P>
 <p>Чем больше друзей приглосите в паутинку, тем
-больше голосов получаете и тем больше PAU у вас будет.</p></td></tr><be/>
+больше голосов получаете и тем больше PAU у вас будет.</p></td></tr><br/>&nbsp;<br/>
 <tr><td colspan="4" align="center" valign="middle">
 <h3>Как это работает?</h3>
 <p>После регистрации Вы получите
